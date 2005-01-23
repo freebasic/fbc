c1451866686c362125e4eca4029a5672bca283de
DrV <i_am_drv@users.sourceforge.net>
2005-01-23 15:22:20 +0000

    calls conio window() routine
    r217

---
+++ FreeBASIC/src/rtlib/dos/libfb_io_viewupdate.c
@@ -0,0 +1,49 @@
+/*
+ *  libfb - FreeBASIC's runtime library
+ *	Copyright (C) 2004-2005 Andre Victor T. Vicentini (av1ctor@yahoo.com.br)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * libfb_io_vwupd.c -- view print update (console, no gfx) for DOS
+ *
+ * chng: jan/2005 written [DrV]
+ *
+ */
+
+#include "fb.h"
+
+#include <conio.h>
+
+/*:::::*/
+void fb_ConsoleViewUpdate( void )
+{
+	int top, bot;
+
+	if (fb_viewTopRow >= 0)
+		top = fb_viewTopRow + 1;
+	else
+		top = 1;
+
+	if (fb_viewBotRow >= 0)
+		bot = fb_viewBotRow + 1;
+	else
+		bot = ScreenRows();
+
+	/* left, top, right, bottom */
+	window(1, top, ScreenCols(), bot);
+
+}
