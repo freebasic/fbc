e37165e37a4bbc46f24f2af846b033091f6b98cc
lillo <lillo@users.sourceforge.net>
2005-02-03 09:52:00 +0000

    Fixed coordinates rounding for values < 0
    r371

--- FreeBASIC/src/gfxlib2/libfb_gfx_coord.c
+++ FreeBASIC/src/gfxlib2/libfb_gfx_coord.c
@@ -35,8 +35,8 @@ void fb_hTranslateCoord(float fx, float fy, int *x, int *y)
 		fy = (fy - fb_mode->win_y) * (fb_mode->view_h - 1) / fb_mode->win_h;
 	}
 	
-	*x = (int)(fx + 0.5);
-	*y = (int)(fy + 0.5);
+	*x = (int)(fx > 0 ? fx + 0.5 : fx - 0.5);
+	*y = (int)(fy > 0 ? fy + 0.5 : fy - 0.5);
 	
 	if ((fb_mode->flags & (WINDOW_ACTIVE | WINDOW_SCREEN)) == (WINDOW_ACTIVE | WINDOW_SCREEN))
 		*y = fb_mode->view_h - 1 - *y;
