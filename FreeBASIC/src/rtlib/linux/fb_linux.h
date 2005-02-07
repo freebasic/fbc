1312426317276281cf21e253a081dd03d809ee3c
lillo <lillo@users.sourceforge.net>
2005-02-07 12:54:00 +0000

    Forgot to add this file
    r392

---
+++ FreeBASIC/src/rtlib/linux/fb_linux.h
@@ -0,0 +1,61 @@
+/*
+ *  libfb - FreeBASIC's runtime library
+ *	Copyright (C) 2004-2005 Andre Victor T. Vicentini (av1ctor@yahoo.com.br)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * fb_linux.h -- linux specific console stuff.
+ *
+ * chng: jan/2005 written [lillo]
+ *
+ */
+
+#ifndef __FB_LINUX_H__
+#define __FB_LINUX_H__
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <termios.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+#define INIT_CONSOLE	1
+#define INIT_XTERM	2
+#define INIT_ETERM	3
+
+
+typedef struct FBCONSOLE
+{
+	int inited;
+	int h_out, h_in;
+	FILE *f_out, *f_in;
+	struct termios old_term_out, old_term_in;
+	int in_flags, old_in_flags;
+	int fg_color, bg_color;
+} FBCONSOLE;
+
+extern FBCONSOLE fb_con;
+
+
+extern int fb_hGetCh();
+
+
+#endif
