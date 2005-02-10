ba7abf90c0cf3d7f22312c7b8de775e339c3972f
v1ctor <v1ctor@users.sourceforge.net>
2005-02-10 04:00:08 +0000

    r412

---
+++ FreeBASIC/src/rtlib/dos/libfb_time_tmr.c
@@ -0,0 +1,45 @@
+/*
+ *  libfb - FreeBASIC's runtime library
+ *	Copyright (C) 2004-2005 Andre Victor T. Vicentini (av1ctor@yahoo.com.br)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * time_tmr.c -- DOS timer# function
+ *
+ * chng: feb/2004 written [v1ctor]
+ *
+ */
+
+#include "fb.h"
+#include <time.h>
+
+
+/*:::::*/
+FBCALL double fb_Timer( void )
+{
+	static long start_time = 0;
+    struct timeval tv;
+        
+    if( start_time == 0 )
+    {
+    	gettimeofday( &tv, NULL );
+        start_time = (tv.tv_sec * 1000000) + tv.tv_usec;
+	}
+        
+    gettimeofday( &tv, NULL );
+    return (double)(((tv.tv_sec * 1000000) + tv.tv_usec) - start_time) / 1000000.0;
+}
