84442be53a405d9e01453c5dc12b327181825036
lillo <lillo@users.sourceforge.net>
2005-03-22 22:47:25 +0000

    Win32 TIMER now uses highres timer when available
    r714

--- FreeBASIC/src/rtlib/win32/libfb_time_tmr.c
+++ FreeBASIC/src/rtlib/win32/libfb_time_tmr.c
@@ -30,12 +30,35 @@
 
 #include <windows.h>
 
+#define TIMER_NONE		0
+#define TIMER_NORMAL	1
+#define TIMER_HIGHRES	2
+
+
+static int timer = TIMER_NONE;
+static double frequency;
+
 
 /*:::::*/
 FBCALL double fb_Timer ( void )
 {
-	/* from msecs to secs */
-	return (double)GetTickCount( ) / 1000.0;
+	LARGE_INTEGER count;
+	
+	if( timer == TIMER_NONE ) {
+		if( QueryPerformanceFrequency( &count ) ) {
+			frequency = 1.0 / (double)count.QuadPart;
+			timer = TIMER_HIGHRES;
+		}
+		else
+			timer = TIMER_NORMAL;
+	}
+	
+	if( timer == TIMER_NORMAL )
+		return (double)GetTickCount( ) * 0.001;
+	else {
+		QueryPerformanceCounter( &count );
+		return (double)count.QuadPart * frequency;
+	}
 }
 
 
