805b39ae609cde32c70b12a84aaa724fef5a15df
Sterling Christensen <stringcheesian@users.sourceforge.net>
2005-03-25 06:26:07 +0000

    Returned without deleting temp str if pos <= 0
    r731

--- FreeBASIC/src/rtlib/libfb_str_asc.c
+++ FreeBASIC/src/rtlib/libfb_str_asc.c
@@ -34,11 +34,11 @@ FBCALL unsigned int fb_ASC ( FBSTRING *str, int pos )
     unsigned int a;
     int len;
 
-	if( str == NULL || pos <= 0 )
+	if( str == NULL )
 		return 0;
 
 	len = FB_STRSIZE( str );
-	if( (str->data == NULL) || (len == 0) || (pos > len) )
+	if( (str->data == NULL) || (len == 0) || (pos <= 0) || (pos > len) )
 		a = 0;
 	else
 		a = (unsigned char)str->data[pos-1];
