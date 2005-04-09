a77cf5ce7fd6bbf800e014a52c915e015e1f756a
v1ctor <v1ctor@users.sourceforge.net>
2005-04-09 21:46:06 +0000

    r882

---
+++ FreeBASIC/install.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set _defpath_=lib\win32\def
+
+rem
+rem create the import-libraries
+rem
+
+if not exist %_defpath_%\genimplibs.exe goto deferr
+
+%_defpath_%\genimplibs.exe
+goto defend
+
+rem ----------------
+:deferr
+echo.
+echo.ERROR: %_defpath_%\genimplibs.exe not found.
+echo.
+goto batexit
+
+rem ----------------
+:defend
+goto batend
+
+rem ----------------
+:batend
+echo.
+echo.Installation completed.
+echo.
+
+rem ----------------
+:batexit
+pause
+
+set _defpath_=
