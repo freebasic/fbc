9d5f0f9a9b7dce2db6b4ff8b17f8b8d785d6fde3
mjs <mjscod@users.sourceforge.net>
2005-07-20 11:29:05 +0000

    * Added lillos hack for CTORS/DTORS on Linux again - but also test for   NULL pointers during CTOR/DTOR execution.
    r1347

--- FreeBASIC/src/rtlib/libfb_init.c
+++ FreeBASIC/src/rtlib/libfb_init.c
@@ -37,6 +37,13 @@ FB_INPCTX fb_inpctx = { 0 };
 FB_PRINTUSGCTX fb_printusgctx = { 0 };
 FB_TLSENTRY fb_dirctx = 0;
 
+/* Dummy function to ensure that FB CTORS/DTORS get really called (on Linux)
+ *
+ * @lillo: any idea why this is needed?
+ */
+static void *dummy_ctor __attribute__ ((section (".fb_ctors"))) = NULL;
+static void *dummy_dtor __attribute__ ((section (".fb_dtors"))) = NULL;
+
 /* prototypes for the FB constructors and destructors */
 typedef void (*FnCTOR)(void);
 typedef void (*FnDTOR)(void);
@@ -50,7 +57,8 @@ void fb_CallCTORS(void)
 {
     FnCTOR *pCTOR;
     for (pCTOR=&__FB_CTOR_BEGIN__; pCTOR!=&__FB_CTOR_END__; ++pCTOR) {
-        (*pCTOR)();
+        if (*pCTOR)
+            (*pCTOR)();
     }
 }
 
@@ -59,7 +67,8 @@ void fb_CallDTORS(void)
 {
     FnDTOR *pDTOR;
     for (pDTOR=&__FB_DTOR_BEGIN__; pDTOR!=&__FB_DTOR_END__; ++pDTOR) {
-        (*pDTOR)();
+        if (*pDTOR)
+            (*pDTOR)();
     }
 }
 
@@ -71,6 +80,11 @@ FBCALL void fb_Init ( int argc, char **argv )
 
 	/* os-dep initialization */
     fb_hInit( argc, argv );
+
+#if 0
+    /* initialize the file handle for direct screen/keyboard access */
+    fb_VfsInitScreen();
+#endif
 
     /* call all freebasic constructor functions */
     fb_CallCTORS();
