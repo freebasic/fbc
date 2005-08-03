148ab2a5dc82a3ebe6b240f3c86eca28caf23d36
lillo <lillo@users.sourceforge.net>
2005-08-03 20:15:38 +0000

    Added DOS ports I/O support
    r1459

---
+++ FreeBASIC/src/rtlib/dos/libfb_sys_ports.c
@@ -0,0 +1,50 @@
+/*
+ *  libfb - FreeBASIC's runtime library
+ *	Copyright (C) 2004-2005 Andre V. T. Vicentini (av1ctor@yahoo.com.br) and others.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * sys_ports.c -- ports I/O for DOS
+ *
+ * chng: aug/2005 written [lillo]
+ *
+ */
+
+#include "fb.h"
+#include "fb_dos.h"
+#include "fb_rterr.h"
+
+
+/*:::::*/
+int fb_hIn( unsigned short port )
+{
+	unsigned char value;
+	
+	__asm__ volatile ("inb %1, %0" : "=a" (value) : "d" (port));
+	
+	return (int)value;
+}
+
+
+/*:::::*/
+int fb_hOut( unsigned short port, unsigned char value )
+{
+	__asm__ volatile ("outb %0, %1" : : "a" (value), "d" (port));
+	
+	return FB_RTERROR_OK;
+}
+
