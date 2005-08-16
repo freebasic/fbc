a7b264a0fb7f70cb76100948dba0ecd5a898a15f
mjs <mjscod@users.sourceforge.net>
2005-08-16 09:47:24 +0000

    * Fix for the open hook when a string (with temp. descr. and static data) was   passed as a file name to OPEN and the hook changes the file name.
    r1567

--- FreeBASIC/src/rtlib/libfb_file_open.c
+++ FreeBASIC/src/rtlib/libfb_file_open.c
@@ -21,6 +21,8 @@
  * file_open - open and core file functions
  *
  * chng: oct/2004 written [marzec/v1ctor]
+ *       jul/2005 modified to use the VFS OPEN method
+ *       aug/2005 added OPEN hook
  *
  */
 
@@ -87,25 +89,33 @@ void fb_hFileCtx ( int doinit )
 int fb_FileOpenEx( FB_FILE *handle, FBSTRING *str_filename, unsigned int mode,
                    unsigned int access, unsigned int lock, int len )
 {
+    int res = 0;
+    FBSTRING str_tmp = { 0 };
     FnFileOpen pfnFileOpen = NULL;
-    if( fb_pfnDevOpenHook!=NULL ) {
-        int res;
-        FBSTRING str_tmp = { 0 };
 
-        /* Create temporary string WITHOUT(!) FB_TEMPSTRBIT set */
-        FB_STRLOCK();
-
-        if( fb_hStrRealloc( &str_tmp, FB_STRSIZE( str_filename ), FALSE )==NULL ) {
-            FB_STRUNLOCK();
-            return fb_ErrorSetNum( FB_RTERROR_OUTOFMEM );
-        }
-
-        fb_hStrCopy( str_tmp.data,
-                     str_filename->data,
-                     FB_STRSIZE( str_filename ) );
-
+    /* Create a modifiable string.
+     *
+     * We have to copy it here because the hook might modify this string
+     * but this is an error when the string passed to OPEN is a temporary
+     * string created from static data (temp. decriptor, static data).
+     *
+     * This could be avoided if we'd have a string handle with a separate
+     * FLAGS member where we'd be able to store some kind of STATIC_DATA
+     * flag so that a call to fb_StrAssign wouldn't try to free this static
+     * data which is always a bad idea.
+     */
+    FB_STRLOCK();
+    if( fb_hStrRealloc( &str_tmp, FB_STRSIZE( str_filename ), FALSE )==NULL ) {
         FB_STRUNLOCK();
+        return fb_ErrorSetNum( FB_RTERROR_OUTOFMEM );
+    }
+    fb_hStrCopy( str_tmp.data,
+                 str_filename->data,
+                 FB_STRSIZE( str_filename ) );
+    fb_hStrDelTemp( str_filename );
+    FB_STRUNLOCK();
 
+    if( fb_pfnDevOpenHook!=NULL ) {
         /* Call the OPEN hook */
         res = fb_pfnDevOpenHook( &str_tmp,
                                  mode,
@@ -113,30 +123,30 @@ int fb_FileOpenEx( FB_FILE *handle, FBSTRING *str_filename, unsigned int mode,
                                  lock,
                                  len,
                                  &pfnFileOpen );
+    }
 
-        /* Release temporary string */
-        FB_STRLOCK();
-
-        fb_StrAssign( str_filename, -1, &str_tmp, -1, FALSE );
-        if( str_tmp.data )
-            free( str_tmp.data );
-
-        FB_STRUNLOCK();
-
-        /* Set error to returned error number */
-        if( res!=0 )
-            return fb_ErrorSetNum( res );
-
+    if( res==0 ) {
         if( pfnFileOpen==NULL ) {
             /* Defaults to "normal" FILE OPEN function */
             pfnFileOpen = fb_DevFileOpen;
         }
+
+        res = fb_FileOpenVfsEx( handle, &str_tmp,
+                                mode, access,
+                                lock, len, pfnFileOpen );
     } else {
-        pfnFileOpen = fb_DevFileOpen;
+        /* Set error to the error number previously returned by the open
+         * hook */
+        fb_ErrorSetNum( res );
     }
-    return fb_FileOpenVfsEx( handle, str_filename,
-                             mode, access,
-                             lock, len, pfnFileOpen );
+
+    /* Release temporary string */
+    FB_STRLOCK();
+    if( str_tmp.data )
+        free( str_tmp.data );
+    FB_STRUNLOCK();
+
+    return res;
 }
 
 /*:::::*/
