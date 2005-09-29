492b6d958fa0e3b4ce59635aa5626dce2c99d9da
v1ctor <v1ctor@users.sourceforge.net>
2005-09-29 21:50:18 +0000

    fixed: integer overflow was happening when near to daily wrap-around (37 bits are need to express 86399 * 1000000), no wrap-around is done now
    r1775

--- FreeBASIC/src/rtlib/dos/libfb_time_tmr.c
+++ FreeBASIC/src/rtlib/dos/libfb_time_tmr.c
@@ -20,13 +20,12 @@
 /*
  * time_tmr.c -- DOS timer# function
  *
- * chng: feb/2004 written [v1ctor]
+ * chng: feb/2005 written [v1ctor]
  *
  */
 
 #include "fb.h"
 #include <time.h>
-
 
 /*:::::*/
 FBCALL double fb_Timer( void )
@@ -34,5 +33,6 @@ FBCALL double fb_Timer( void )
         struct timeval tv;
         
         gettimeofday(&tv, NULL);
-        return (double)(((tv.tv_sec % 86400) * 1000000) + tv.tv_usec) / 1000000.0;
+        
+        return ( ((double)tv.tv_sec * 1000000.0) + (double)tv.tv_usec) * 0.000001;
 }
