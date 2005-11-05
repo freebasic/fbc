d7442b593ae886e6b69d7e39df816b6dd19f76e3
v1ctor <v1ctor@users.sourceforge.net>
2005-11-05 17:04:17 +0000

    r1941

---
+++ FreeBASIC/src/contrib/wikiconv/common.cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+	define( 'CFG_DB_NAME',		'' );
+	define( 'CFG_DB_HOST',		'' );
+	define( 'CFG_DB_USER',		'' );
+	define( 'CFG_DB_PWD',		'' );
+	
+	define( 'CFG_PATH_HTML', 	'html' );
+
+?>
