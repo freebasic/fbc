13b35b2bf5d665d7cc1490b6b6901e09070196da
v1ctor <v1ctor@users.sourceforge.net>
2006-06-11 21:44:37 +0000

    fixed: null chars in fixed-len strings were been printed
    r2780

--- FreeBASIC/src/rtlib/linux/libfb_io_printbuff.c
+++ FreeBASIC/src/rtlib/linux/libfb_io_printbuff.c
@@ -49,9 +49,8 @@
 void fb_ConsolePrintBufferEx( const void *buffer, size_t len, int mask )
 {
 	size_t avail, avail_len;
-	const unsigned char *c = (const unsigned char *) buffer;
-	const char *code;
-	int i;
+	const unsigned char *cbuffer = (const unsigned char *) buffer;
+	unsigned int c;
 	
     if (!fb_con.inited) {
         fwrite(buffer, len, 1, stdout);
@@ -69,32 +68,37 @@ void fb_ConsolePrintBufferEx( const void *buffer, size_t len, int mask )
 	memcpy(fb_con.char_buffer + ((fb_con.cur_y - 1) * fb_con.w) + fb_con.cur_x - 1, buffer, avail_len);
 	memset(fb_con.attr_buffer + ((fb_con.cur_y - 1) * fb_con.w) + fb_con.cur_x - 1, fb_con.fg_color | (fb_con.bg_color << 4), avail_len);
 	
-	for (; len; len--, c++) {
-		if (*c < 32) {
-			if ((CTRL_ALWAYS >> *c) & 0x1) {
+	for (; len; len--, cbuffer++) {
+		c = *cbuffer;
+		if( c == 0 ) 
+			c = 32;
+
+		if (c < 32) {
+			if ((CTRL_ALWAYS >> c) & 0x1) {
 				/* This character can't be printed, we must use unicode
 				 * Enter UTF-8 and start constructing 0xF000 code
 				 */
 				fputs(ENTER_UTF8 "\xEF\x80", fb_con.f_out);
 				/* Set the last 6 bits */
-				fputc(*c | 0x80, fb_con.f_out);
+				fputc(c | 0x80, fb_con.f_out);
 				/* Escape UTF-8 */
 				fputs(EXIT_UTF8, fb_con.f_out);
 			}
 			else
-				fputc(*c, fb_con.f_out);
+				fputc( c, fb_con.f_out);
 		}
 		else
-			fputc(*c, fb_con.f_out);
+			fputc(c, fb_con.f_out);
 		
 		fb_con.cur_x++;
-		if ((*c == 10) || (fb_con.cur_x >= fb_con.w)) {
+		if ((c == 10) || (fb_con.cur_x >= fb_con.w)) {
 			fb_con.cur_x = 1;
 			fb_con.cur_y++;
 			if (fb_con.cur_y > fb_con.h)
 				fb_con.cur_y = fb_con.h;
 		}
 	}
+
 	fflush(fb_con.f_out);
 }
 
