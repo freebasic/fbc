4e2daaeed3e4662a886a39cde2b2d29aa55b9473
DrV <i_am_drv@users.sourceforge.net>
2006-10-29 09:43:06 +0000

    changed: SetDate/SetTime now return an error when they fail (request 1583292)
    r3392

--- FreeBASIC/src/rtlib/libfb_time_timeset.c
+++ FreeBASIC/src/rtlib/libfb_time_timeset.c
@@ -55,45 +55,54 @@ FBCALL int fb_SetTime( FBSTRING *time )
 	if( (time != NULL) && (time->data != NULL) )
 	{
 
-    	char *t, c;
-    	int i, h, m = 0, s = 0;
+		char *t, c;
+		int i, h, m = 0, s = 0;
 
-    	/* get hours */
-    	h = 0;
-		for (i = 0, t = time->data; (c = *t) && isdigit(c); t++, i += 10)
+		/* get hours */
+		h = 0;
+		for( i = 0, t = time->data; (c = *t) && isdigit(c); t++, i += 10 )
+		{
 			h = h * i + c - '0';
+		}
 
-		if( (h > 23) || (c != '\0' && c != ':') ) {
+		if( (h > 23) || (c != '\0' && c != ':') )
+		{
 			return fb_ErrorSetNum( FB_RTERROR_ILLEGALFUNCTIONCALL );
 		}
 
-    	if (c != '\0')
-    	{
+		if( c != '\0' )
+		{
 			/* get minutes */
-     		m = 0;
-     		for (i = 0, t++; (c = *t) && isdigit(c); t++, i += 10)
-        		 m = m * i + c - '0';
+			m = 0;
+			for( i = 0, t++; (c = *t) && isdigit(c); t++, i += 10 )
+			{
+				 m = m * i + c - '0';
+			}
 
-     		if( (m > 59) || (c != '\0' && c != ':') ) {
-     			return fb_ErrorSetNum( FB_RTERROR_ILLEGALFUNCTIONCALL );
-     		}
+			if( (m > 59) || (c != '\0' && c != ':') )
+			{
+				return fb_ErrorSetNum( FB_RTERROR_ILLEGALFUNCTIONCALL );
+			}
 
-     		if (c != '\0')
-     		{
-     			/* get seconds */
-     			s = 0;
-     			for (i = 0, t++; (c = *t) && isdigit(c); t++, i += 10)
-         			s = s * i + c - '0';
-    		}
-    	}
+			if( c != '\0' )
+			{
+				/* get seconds */
+				s = 0;
+				for (i = 0, t++; (c = *t) && isdigit(c); t++, i += 10)
+					s = s * i + c - '0';
+			}
+		}
 
-    	if ((s > 59) || (c != '\0')) {
+		if( (s > 59) || (c != '\0') )
+		{
 			return fb_ErrorSetNum( FB_RTERROR_ILLEGALFUNCTIONCALL );
 		}
 
 
-		fb_hSetTime( h, m, s );
-
+		if ( fb_hSetTime( h, m, s ) )
+		{
+			return fb_ErrorSetNum( FB_RTERROR_ILLEGALFUNCTIONCALL );	
+		}
 	}
 
 	/* del if temp */
--- FreeBASIC/src/rtlib/win32/libfb_time_settime.c
+++ FreeBASIC/src/rtlib/win32/libfb_time_settime.c
@@ -43,22 +43,25 @@
 /*:::::*/
 int fb_hSetTime( int h, int m, int s )
 {
-    SYSTEMTIME st;
+	SYSTEMTIME st;
 
 	/* get current local time and date */
-   	GetLocalTime(&st);
+	GetLocalTime( &st );
 
-   	/* set time fields */
-   	st.wHour = h;
-   	st.wMinute = m;
-   	st.wSecond = s;
+	/* set time fields */
+	st.wHour = h;
+	st.wMinute = m;
+	st.wSecond = s;
 
-   	/* set system time relative to local time zone */
-   	SetLocalTime(&st);
+	/* set system time relative to local time zone */
+	if( SetLocalTime( &st ) == 0 )
+	{
+		return -1;
+	}
 
-   	/* send WM_TIMECHANGE to all top-level windows on NT and 95/98/Me
-   	* (_not_ on 2K/XP etc.) */
-   	/*if ((GetVersion() & 0xFF) == 4)
+	/* send WM_TIMECHANGE to all top-level windows on NT and 95/98/Me
+	 * (_not_ on 2K/XP etc.) */
+	/*if ((GetVersion() & 0xFF) == 4)
 		SendMessage(HWND_BROADCAST, WM_TIMECHANGE, 0, 0);*/
 
 	return 0;
