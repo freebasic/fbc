06ebc10999a91a0b2e8b44030921b5884f9c3abf
coderJeff <jeffmarshall@users.sourceforge.net>
2006-11-06 16:32:37 +0000

    added fb_hDynLoadAlso() for win32
    r3426

--- FreeBASIC/src/rtlib/win32/libfb_hdynload.c
+++ FreeBASIC/src/rtlib/win32/libfb_hdynload.c
@@ -62,6 +62,22 @@ FB_DYLIB fb_hDynLoad(const char *libname, const char **funcname, void **funcptr)
 
 
 /*:::::*/
+int fb_hDynLoadAlso(FB_DYLIB lib, const char **funcname, void **funcptr, int count)
+{
+	int i;
+	
+	/* Load functions */
+	for (i = 0; i < count; i++) {
+		funcptr[i] = GetProcAddress(lib, funcname[i]);
+		if (!funcptr[i])
+			return -1;
+	}
+	
+	return 0;
+}
+
+
+/*:::::*/
 void fb_hDynUnload(FB_DYLIB *lib)
 {
     if (*lib) {
