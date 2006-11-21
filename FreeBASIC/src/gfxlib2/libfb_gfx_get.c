405b2c225084d8e0e01e39b015aa7df32be1f468
lillo <lillo@users.sourceforge.net>
2006-11-21 12:52:57 +0000

    use old-style get/put header only if -lang qb or deprecated is specified
    r3485

--- FreeBASIC/src/gfxlib2/libfb_gfx_get.c
+++ FreeBASIC/src/gfxlib2/libfb_gfx_get.c
@@ -58,7 +58,7 @@ FBCALL int fb_GfxGet(void *target, float fx1, float fy1, float fx2, float fy2, u
 			return fb_ErrorSetNum(FB_RTERROR_ILLEGALFUNCTIONCALL);
 
 	header = (PUT_HEADER *)dest;
-	if (__fb_gfx->bpp == 1) {
+	if (__fb_gfx->flags & QB_COMPATIBILITY) {
 		/* use old-style header for compatibility */
 		header->old.bpp = __fb_gfx->bpp;
 		header->old.width = w;
