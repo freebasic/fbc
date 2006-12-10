46fcd06dd08adba3a588f28699fa500c7c6c8f6c
lillo <lillo@users.sourceforge.net>
2006-12-10 18:44:43 +0000

    fixed bug #1610260
    r3589

--- FreeBASIC/src/gfxlib2/libfb_gfx_image.c
+++ FreeBASIC/src/gfxlib2/libfb_gfx_image.c
@@ -78,6 +78,7 @@ FBCALL void *fb_GfxImageCreate(int width, int height, unsigned int color, int de
 		image->pitch = pitch;
 		fb_hMemSet(image->_reserved, 0, sizeof(image->_reserved));
 	}
+	fb_hPrepareTarget(context, (void *)image, MASK_A_32);
 	context->pixel_set((unsigned char *)image + header_size, color, (pitch * height) / bpp);
 	
 	return image;
