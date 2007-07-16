4e0535ac34220e01bd01a2bdd33c91c9ec9e3aef
DrV <i_am_drv@users.sourceforge.net>
2007-07-16 21:21:02 +0000

    split generic unix code from linux-specific code in rtlib
    r4029

--- FreeBASIC/src/rtlib/linux/libfb_sys_fmem.c
+++ FreeBASIC/src/rtlib/linux/libfb_sys_fmem.c
@@ -37,13 +37,14 @@
  */
 
 #include "fb.h"
+#include <unistd.h>
 #include <sys/sysinfo.h>
 
 /*:::::*/
 FBCALL unsigned int fb_GetMemAvail ( int mode )
 {
 
-    return get_avphys_pages() * 4096;
+    return get_avphys_pages() * sysconf(_SC_PAGE_SIZE);
 
 }
 
