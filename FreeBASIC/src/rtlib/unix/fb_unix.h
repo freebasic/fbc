3ced668ad143dbd1520d793771bd2bf7a777b7f6
coderJeff <jeffmarshall@users.sourceforge.net>
2007-07-21 23:47:13 +0000

    File needed to build on linux - not exactly sure what's supposed to be in here yet though
    r4037

---
+++ FreeBASIC/src/rtlib/unix/fb_unix.h
@@ -0,0 +1,12 @@
+#ifndef __FB_UNIX_H__
+#define __FB_UNIX_H__
+
+/*
+  probably some stuff is going to get split from linux/fb_linux.h
+  an put here, but it's not been done yet.
+
+  So, this file just exists for now to make the build scripts work.
+
+*/
+
+#endif
