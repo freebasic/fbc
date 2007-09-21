ee7d615b1ccecc3f8609da01ba581d345c7d8107
cha0s <fbc@therealcha0s.net>
2007-09-21 12:36:06 +0000

    fixed: #1799517 - VAL() was evaluating out-of-bound characters impromperly in non-base 10 numbers.
    r4173

--- FreeBASIC/src/rtlib/libfb_str_convfrom_rad.c
+++ FreeBASIC/src/rtlib/libfb_str_convfrom_rad.c
@@ -45,26 +45,40 @@ FBCALL int fb_hStrRadix2Int( char *src, int len, int radix )
 		case 16:
 			while( --len >= 0 )
 			{
-				c = (int)*src++ - 48;
-                if( c > 9 )
-                	c -= (65 - 57 - 1);
-				if( c > 16 )
-					c -= (97 - 65);
-
+				c = (int)*src++;
+				if( (c >= 97) && (c <= 102) )
+					c -= 87;
+				else if( (c >= 65) && (c <= 70) )
+					c -= 55;
+				else if( (c >= 48) && (c <= 57) )
+					c -= 48;
+				else
+					break;
+				
 				v = (v * 16) + c;
 			}
 			break;
 
 		/* oct */
 		case 8:
-			while( --len >= 0 )
-				v = (v * 8) + ((int)*src++ - 48);
+			while( --len >= 0 ) {
+				c = (int)*src++;
+				if( (c >= 48) && (c <= 55) )
+					v = (v * 8) + (c - 48);
+				else
+					break;
+			}
 			break;
 
 		/* bin */
 		case 2:
-			while( --len >= 0 )
-				v = (v * 2) + ((int)*src++ - 48);
+			while( --len >= 0 ) {
+				c = (int)*src++;
+				if( (c >= 48) && (c <= 49) )
+					v = (v * 2) + (c - 48);
+				else
+					break;
+			}
 			break;
 	}
 
