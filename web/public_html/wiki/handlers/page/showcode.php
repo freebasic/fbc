c7eb0feed8e0590a7644c6f5aae1c44ed17bb269
coderJeff <jeffmarshall@users.sourceforge.net>
2007-12-28 01:15:22 +0000

    move 0.18.1 tag dir
    r4445

--- web/public_html/wiki/handlers/page/showcode.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($this->HasAccess("read") && $this->page) {
-	// display raw page, slightly formatted for viewing
-	print(nl2br($this->htmlspecialchars_ent($this->page["body"], ENT_QUOTES)));
-}
-?>
