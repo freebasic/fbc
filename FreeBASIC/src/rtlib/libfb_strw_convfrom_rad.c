e027e99ae032ba23ca4b5aee8f803e6edd4fce3c
counting_pine <counting_pine@users.sourceforge.net>
2008-03-26 00:32:30 +0000

    More VAL* invalid character issues
    r4681

--- FreeBASIC/src/rtlib/libfb_strw_convfrom_rad.c
+++ FreeBASIC/src/rtlib/libfb_strw_convfrom_rad.c
@@ -32,7 +32,7 @@
 #include "fb.h"
 
 /*:::::*/
-FBCALL int fb_WstrRadix2Int( const FB_WCHAR *s, int len, int radix )
+FBCALL int fb_WstrRadix2Int( const FB_WCHAR *src, int len, int radix )
 {
 	int c, v;
 
@@ -44,26 +44,40 @@ FBCALL int fb_WstrRadix2Int( const FB_WCHAR *s, int len, int radix )
 		case 16:
 			while( --len >= 0 )
 			{
-				c = (int)(*s++) - 48;
-                if( c > 9 )
-                	c -= (65 - 57 - 1);
-				if( c > 16 )
-					c -= (97 - 65);
-
+				c = (int)*src++;
+				if( (c >= 97) && (c <= 102) )
+					c -= 87;
+				else if( (c >= 65) && (c <= 70) )
+					c -= 55;
+				else if( (c >= 48) && (c <= 57) )
+					c -= 48;
+				else
+					break;
+				
 				v = (v * 16) + c;
 			}
 			break;
 
 		/* oct */
 		case 8:
-			while( --len >= 0 )
-				v = (v * 8) + ((int)(*s++) - 48);
+			while( --len >= 0 ) {
+				c = (int)*src++;
+				if( (c >= 48) && (c <= 55) )
+					v = (v * 8) + (c - 48);
+				else
+					break;
+			}
 			break;
 
 		/* bin */
 		case 2:
-			while( --len >= 0 )
-				v = (v * 2) + ((int)(*s++) - 48);
+			while( --len >= 0 ) {
+				c = (int)*src++;
+				if( (c >= 48) && (c <= 49) )
+					v = (v * 2) + (c - 48);
+				else
+					break;
+			}
 			break;
 	}
 
