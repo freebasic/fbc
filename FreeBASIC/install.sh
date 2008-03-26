9864e6d4145206adb88190a8319df6f90b58a8c1
coderJeff <jeffmarshall@users.sourceforge.net>
2008-03-26 01:55:57 +0000

    changed: - check if grep prefix returns anything - warn if the user specified dest doesn't match the configured prefix, but allow it anyway (don't override the user) - install man page to PREFIX/man/man1
    r4683

--- FreeBASIC/install.sh
+++ FreeBASIC/install.sh
@@ -6,10 +6,11 @@ usage()
 	cat <<EOF
 	
 Usage: 
-./install.sh -i
+./install.sh -i [dest]
 ./install.sh -u [dest]
 
--i	Install FreeBASIC at the compiler configured prefix
+-i	Install FreeBASIC at the compiler configured prefix, unless dest is 
+specified, then PREFIX=dest.
 -u	Uninstall FreeBASIC from the compiler configured prefix, unless dest is 
 specified, then PREFIX=dest.
 
@@ -19,7 +20,7 @@ library and header files are copied to, respectively.
 If necessary, PREFIX/bin will be created. A copy (not a symlink) of fbc will 
 be placed at PREFIX/bin
 
-The man page is copied to PREFIX/share/man/man1. 
+The man page is copied to PREFIX/man/man1. 
 
 Uninstallation removes these directories, and their contents, in addition 
 to the man file and fbc.
@@ -45,14 +46,14 @@ install()
 	mkdir -p -m 0755 "$INSTALLDIR"/share/freebasic && \
 	mkdir -p -m 0755 "$INSTALLDIR"/share/freebasic/docs && \
 	mkdir -p -m 0755 "$INSTALLDIR"/share/freebasic/examples && \
-	mkdir -p -m 0755 "$INSTALLDIR"/share/man/man1 && \
+	mkdir -p -m 0755 "$INSTALLDIR"/man/man1 && \
 	cp lib/linux/* "$INSTALLDIR"/lib/freebasic/ && \
 	cp -ru inc/* "$INSTALLDIR"/include/freebasic/ && \
 	cp -ru examples/* "$INSTALLDIR"/share/freebasic/examples/ && \
 	cp -ru docs/* "$INSTALLDIR"/share/freebasic/docs/ && \
 	cp -ru readme.txt "$INSTALLDIR"/share/freebasic/docs/ && \
 	cp -ru migrating.txt "$INSTALLDIR"/share/freebasic/docs/ && \
-	gzip -c docs/fbc.1 > "$INSTALLDIR"/share/man/man1/fbc.1.gz && \
+	gzip -c docs/fbc.1 > "$INSTALLDIR"/man/man1/fbc.1.gz && \
 	cp fbc "$INSTALLDIR"/bin/ && \
 	chmod a+x "$INSTALLDIR"/bin/fbc && \
 	i_success_msg
@@ -71,7 +72,7 @@ uninstall()
 	rm -fr "$INSTALLDIR"/lib/freebasic && \
 	rm -fr "$INSTALLDIR"/include/freebasic && \
 	rm -f "$INSTALLDIR"/bin/fbc && \
-	rm -f "$INSTALLDIR"/share/man/man1/fbc.1.gz && \
+	rm -f "$INSTALLDIR"/man/man1/fbc.1.gz && \
 	u_success_msg
 }
 
@@ -81,15 +82,31 @@ case "$1" in
 	*)		usage;;
 esac
 
+if [ ! -e ./fbc ]; then
+	echo
+	echo "ERROR: Unable to find ./fbc"
+	echo
+	exit 1
+fi
+
 INSTALLDIR=`./fbc -version | grep prefix | cut -c24- -`
+if [ "$INSTALLDIR" == "" ]; then
+	if [ "$2" == "" ]; then
+		echo
+		echo "ERROR: Unable to determine the configured prefix from ./fbc -version"
+		echo "ERROR: Need destination path"
+		echo
+		exit 1
+	fi
+	INSTALLDIR="$2"
+fi
+
 if [ "$2" != "" ]; then
-	if [ "$ACTION" = "install" ]; then
-		echo "WARNING: The path is assumed from ./fbc, setting installation path to ["$2"] is being ignored..."
-	else
+	if [ "$INSTALLDIR" != "$2" ]; then
+		echo "WARNING: Using destination ["$2"] even though configured prefix is ["$INSTALLDIR"]"
 		INSTALLDIR="$2"
 	fi
 fi
-
 
 if [ ! -d $INSTALLDIR ]; then
 	echo
@@ -104,7 +121,6 @@ if [ ! -w $INSTALLDIR ]; then
 	echo
 	exit 1
 fi
-
 
 case $ACTION in
 	"install")	install;;
