07bbd3dd65b8945ffbd3a56054ea1e89e78a3b4e
coderJeff <jeffmarshall@users.sourceforge.net>
2008-05-31 14:25:25 +0000

    tag v0_20-pre-SSE
    r4850

--- web/public_html/wiki/actions/wikkachanges.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$output = "=====Wikka Release Notes=====\n\n".
-"This server is running [[http://wikka.jsnx.com/ Wikka Wiki]] version **{{wikkaversion}}**.\n".
-"The release notes are described on the [[http://wikka.jsnx.com/WikkaReleaseNotes main Wikka server]].";
-
-print $this->Format($output);
-
-?>
