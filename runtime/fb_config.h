c87c61e07b90c4a2173b4227ef002f86d32df5d8
dkl <daniel.c.klauer@web.de>
2011-07-29 19:24:00 +0200

    Replace the HAVE_FSEEKO[64] checks with host-specific checks

--- runtime/fb_config.h
+++ runtime/fb_config.h
@@ -1,17 +1,15 @@
 #ifndef __FB_CONFIG_H__
 #define __FB_CONFIG_H__
 
-
-#if !defined(HAVE_FSEEKO)
- #undef fseeko
- #undef ftello
- #if defined(HAVE_FSEEKO64)
-  #define fseeko(x, y, z) fseeko64(x, y, z)
-  #define ftello(x)       ftello64(x)
- #else
-  #define fseeko(x, y, z) fseek(x, y, z)
-  #define ftello(x)       ftell(x)
- #endif
+#ifdef HOST_MINGW
+	/* MinGW doesn't recognize _FILE_OFFSET_BITS, so we this manually */
+	#define fseeko(x, y, z) fseeko64(x, y, z)
+	#define ftello(x)       ftello64(x)
+#elif defined(HOST_DOS)
+	/* On DJGPP we didn't use fseeko() at all (the DJGPP semi-2.04 setup
+	   used for FB doesn't seem to have it) */
+	#define fseeko(x, y, z) fseek(x, y, z)
+	#define ftello(x)       ftell(x)
 #endif
 
 #endif /* __FB_CONFIG_H__ */
