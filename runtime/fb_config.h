13bb62044e4207afa7822de11aad1b395da3bbf8
dkl <daniel.c.klauer@web.de>
2011-08-10 03:06:48 +0200

    Follow up _NO_OLDNAMES: strdup and strcasecmp need to be remapped too
    These two weren't remapped in the rtlib before, so maybe now libmoldname
    is no longer needed for FB programs?
    
    For clarification: Before commit 7e1ea36, _NO_OLDNAMES was being #defined
    inside fb_win32.h, which is #included into fb.h, and that often was too
    late, since many modules #include <string.h> (and others) before fb.h.

--- runtime/fb_config.h
+++ runtime/fb_config.h
@@ -8,15 +8,19 @@
 
 	/* We want to use _mkdir() instead of mkdir() (see config.h.in),
 	   so we just #define mkdir to _mkdir */
+	#define chdir _chdir
 	/* Note the special case for mkdir: the second parameter will be
 	   ignored, since the Windows function doesn't have it */
-	#define chdir _chdir
 	#define mkdir(x, y) _mkdir(x)
 	#define pclose _pclose
 	#define popen _popen
 	#define putenv _putenv
 	#define rmdir _rmdir
 	#define snprintf _snprintf
+	#define strdup _strdup
+	/* MinGW's string.h would also do this, if _NO_OLDNAMES would be
+	   defined, just via inline functions */
+	#define strcasecmp _stricmp
 	#define strncasecmp _strnicmp
 #elif defined(HOST_DOS)
 	/* In DJGPP we didn't use fseeko() at all (the DJGPP semi-2.04 setup
