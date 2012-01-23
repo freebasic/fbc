0f16963937146791ef94da0fb6e50c31134ab567
Ebben Feagan <sir_mud@users.sourceforge.net>
2012-01-23 04:13:23 -0600

    Updated manifests and includes.* Removed headers from platform list if they are not supported on the platform. includes.cmn = supported on all platforms includes.mod = supported on modern platforms only (windows/*nix) includes.dos = DOS only includes.win = Win32 only includes.nix = *nix only

--- includes.nix
+++ includes.nix
@@ -7,9 +7,13 @@ include/crt/linux/stdlib.bi \
 include/crt/linux/time.bi \
 include/crt/linux/unistd.bi \
 include/crt/linux/wchar.bi \
+include/crt/netdb.bi \
+include/crt/netinet/in.bi \
+include/crt/netinet/linux/in.bi \
 include/crt/sys/linux/select.bi \
 include/crt/sys/linux/socket.bi \
 include/crt/sys/linux/time.bi \
-include/crt/sys/linux/types.bi
+include/crt/sys/linux/types.bi \
+include/curses/ncurses.bi
 
 
