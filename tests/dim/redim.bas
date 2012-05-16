freebasic/fbc

commit 83286fa701fa362f9846e527646a5c331e27ac2c

fixed: DIM wasn't being allowed after COMMON Made full use of some helper macros
r4228

FreeBASIC/src/compiler/parser-decl-var.bas
@@ -551,8 +551,8 @@ private function hDeclDynArray _
 			elseif( dimensions = -1 ) then
 				sym = NULL
 
-			'' dim foo(variable)?
-			elseif( is_redim = FALSE ) then
+			'' dim foo(variable)? (without a preceeding COMMON)
+			elseif( (is_redim = FALSE) and (symbIsCommon( sym ) = FALSE) ) then
 				sym = NULL
 			end if
 		end if
@@ -601,7 +601,11 @@ private function hDeclDynArray _
 
 	'' if COMMON, check for max dimensions used
 	if( (attrib and FB_SYMBATTRIB_COMMON) <> 0 ) then
-		if( dimensions > symbGetArrayDimensions( sym ) ) then
+		if( symbGetArrayDimensions( sym ) > -1 ) then
+    		errReportEx( FB_ERRMSG_DUPDEFINITION, *id )
+    		'' no error recovery, caller will take care of that
+    		exit function
+    	else
 			symbSetArrayDimensions( sym, dimensions )
 		end if
 

FreeBASIC/src/compiler/symb-struct.bas
@@ -365,8 +365,8 @@ function symbAddField _
 	sym->var_.array.dif = symbCalcArrayDiff( dimensions, dTB(), lgt )
 	sym->var_.array.dimhead = NULL
 	sym->var_.array.dimtail = NULL
-
-	sym->var_.array.dims = dimensions
+	
+	symbSetArrayDimensions( sym, dimensions )
 	if( dimensions > 0 ) then
 		dim as integer i
 		for i = 0 to dimensions-1

FreeBASIC/src/compiler/symb-var.bas
@@ -293,7 +293,7 @@ function symbAddArrayDesc _
 	'' as desc is also a var, clear the var fields
 	desc->var_.array.desc = NULL
 	desc->var_.array.dif = 0
-	desc->var_.array.dims = 0
+	symbSetArrayDimensions( desc, 0 )
 	desc->var_.array.dimhead = NULL
 	desc->var_.array.dimtail = NULL
 	desc->var_.array.elms = 1
@@ -358,7 +358,7 @@ sub symbSetArrayDimTb _
 		s->var_.array.dif = symbCalcArrayDiff( dimensions, dTB(), s->lgt )
 
 		if( (s->var_.array.dimhead = NULL) or _
-			(s->var_.array.dims <> dimensions) ) then
+			(symbGetArrayDimensions( s ) <> dimensions) ) then
 
             hDelVarDims( s )
 
@@ -382,8 +382,8 @@ sub symbSetArrayDimTb _
 		s->var_.array.dif = 0
 		s->var_.array.elms = 1
 	end if
-
-	s->var_.array.dims = dimensions
+	
+    symbSetArrayDimensions( s, dimensions )
 
 	'' dims can be -1 with COMMON arrays..
 	if( dimensions <> 0 ) then
@@ -431,7 +431,7 @@ private sub hSetupVar _
 	if( dimensions <> 0 ) then
 		symbSetArrayDimTb( s, dimensions, dTB() )
 	else
-		s->var_.array.dims = 0
+		symbSetArrayDimensions( s, 0 )
 		s->var_.array.dif = 0
 		s->var_.array.elms = 1
 	end if
@@ -839,7 +839,7 @@ private sub hDelVarDims _
 
     s->var_.array.dimhead = NULL
     s->var_.array.dimtail = NULL
-    s->var_.array.dims	  = 0
+    symbSetArrayDimensions( s, 0 )
 
 end sub
 
@@ -854,7 +854,7 @@ sub symbDelVar _
     	exit sub
     end if
 
-    if( s->var_.array.dims > 0 ) then
+    if( symbGetArrayDimensions( s ) > 0 ) then
     	hDelVarDims( s )
     	if( is_tbdel = FALSE ) then
     		'' del the array descriptor, recursively

FreeBASIC/src/compiler/symb.bas
@@ -1538,7 +1538,7 @@ function symbIsArray _
 		if( (sym->attrib and (FB_SYMBATTRIB_DYNAMIC or FB_SYMBATTRIB_PARAMBYDESC)) <> 0 ) then
 			return TRUE
 		else
-			return sym->var_.array.dims <> 0
+			return symbGetArrayDimensions( sym ) <> 0
 		end if
 	end select
 

commit 0850a9edcc4021fac6fac6342899d3406755ba09

Fix #3474348: Multiple REDIMs on COMMON array were disallowed
This was a regression from 83286fa701fa362f9846e527646a5c331e27ac2c.

compiler/parser-decl-var.bas
@@ -450,22 +450,18 @@ private function hDeclDynArray _
    			'' could be an external..
    			sym = hDeclExternVar( sym, id, dtype, subtype, attrib, addsuffix, _
    								  dimensions, dTB() )
-
 		else
 			'' var already exists; dup checks
 
 			'' EXTERNal?
 			if( symbIsExtern( sym ) ) then
-
 				'' another EXTERN? (declared twice)
 				if( (attrib and FB_SYMBATTRIB_EXTERN) <> 0 ) then
    					sym = NULL
-
 				else
 	   				'' define it...
 					hVarExtToPub( sym, attrib )
 				end if
-
 			'' [re]dim ()?
 			elseif( dimensions = -1 ) then
 				sym = NULL
@@ -497,48 +493,33 @@ private function hDeclDynArray _
 		return sym
 	end if
 
-	'' not an argument passed by descriptor or a common array?
+	if( (dtype <> symbGetFullType( sym )) or _
+	    (subtype <> symbGetSubType( sym )) ) then
+		errReportEx( FB_ERRMSG_DUPDEFINITION, *id )
+		'' no error recovery, caller will take care of that
+		exit function
+	end if
+
+	'' Check dimensions, unless it's a bydesc param or a common array,
+	'' then we don't know the dimensions at compile-time.
 	if( (attrib and (FB_SYMBATTRIB_PARAMBYDESC or FB_SYMBATTRIB_COMMON)) = 0 ) then
-
-		if( (dtype <> symbGetFullType( sym )) or _
-			(subtype <> symbGetSubType( sym )) ) then
-    		errReportEx( FB_ERRMSG_DUPDEFINITION, *id )
-    		'' no error recovery, caller will take care of that
-    		exit function
-		end if
-
 		if( symbGetArrayDimensions( sym ) > 0 ) then
 			if( dimensions <> symbGetArrayDimensions( sym ) ) then
-    			errReportEx( FB_ERRMSG_WRONGDIMENSIONS, *id )
-    			'' no error recovery, ditto
-    			exit function
-    		end if
-		end if
-
-	'' else, can't check it's dimensions at compile-time
-	else
-		if( (dtype <> symbGetFullType( sym )) or _
-			(subtype <> symbGetSubType( sym )) ) then
-    		errReportEx( FB_ERRMSG_DUPDEFINITION, *id )
-    		'' no error recovery, ditto
-    		exit function
+				errReportEx( FB_ERRMSG_WRONGDIMENSIONS, *id )
+				'' no error recovery, ditto
+				exit function
+			end if
 		end if
 	end if
 
 	'' if COMMON, check for max dimensions used
 	if( (attrib and FB_SYMBATTRIB_COMMON) <> 0 ) then
-		if( symbGetArrayDimensions( sym ) > -1 ) then
-    		errReportEx( FB_ERRMSG_DUPDEFINITION, *id )
-    		'' no error recovery, caller will take care of that
-    		exit function
-    	else
+		if( dimensions > symbGetArrayDimensions( sym ) ) then
 			symbSetArrayDimensions( sym, dimensions )
 		end if
-
 	'' or if dims = -1 (cause of "DIM|REDIM array()")
 	elseif( symbGetArrayDimensions( sym ) = -1 ) then
 		symbSetArrayDimensions( sym, dimensions )
-
 	end if
 
     function = sym

tests/dim/redim.bas
@@ -54,6 +54,33 @@ sub test cdecl
 	CU_ASSERT_EQUAL( ubound(foo), 4 )
 end sub
 
+'' Regression test for #3474348
+namespace commonRedimRedim
+	common array() as integer
+
+	private sub test cdecl( )
+		redim array(0 to 4) as integer
+		CU_ASSERT( lbound( array ) = 0 )
+		CU_ASSERT( ubound( array ) = 4 )
+
+		redim array(0 to 9) as integer
+		CU_ASSERT( lbound( array ) = 0 )
+		CU_ASSERT( ubound( array ) = 9 )
+	end sub
+end namespace
+
+namespace commonDim
+	common array() as integer
+
+	private sub test cdecl( )
+		'' Dim is allowed after Common -- as in QB.
+		dim array(0 to 4) as integer
+
+		CU_ASSERT( lbound( array ) = 0 )
+		CU_ASSERT( ubound( array ) = 4 )
+	end sub
+end namespace
+
 '' Regression test for #3510684: An array using a different mangling, which
 '' affects the array descriptor too -- should still work just the same.
 extern "C"
@@ -74,6 +101,8 @@ private sub ctor( ) constructor
 	fbcu.add_suite("fbc_tests.dim.redim")
 	fbcu.add_test("test", @test)
 	fbcu.add_test("test4", @test4)
+	fbcu.add_test( "Common/Redim/Redim", @commonRedimRedim.test )
+	fbcu.add_test( "Common/Dim", @commonDim.test )
 	fbcu.add_test( "array + desc using non-default mangling", @testMangled )
 end sub
 