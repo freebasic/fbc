97f9856326995f71b7d08e621c777e21634bd8e1
dkl <daniel.c.klauer@web.de>
2012-08-14 22:43:01 +0200

    musl-fbc build script updates: proper /usr prefixes, debug build option

--- contrib/build-musl-fbc.sh
+++ contrib/build-musl-fbc.sh
@@ -2,6 +2,20 @@
 set -ex
 
 BASEDIR=$PWD
+ENABLE_DEBUG=""
+
+if [ "x" != "x$ENABLE_DEBUG" ]; then
+  BASIC_CFLAGS="-g -O2"
+else
+  BASIC_CFLAGS="-O2"
+fi
+
+if [ "x" != "x$ENABLE_DEBUG" ]; then
+  BASIC_FBFLAGS="-g -exx"
+else
+  BASIC_FBFLAGS=""
+fi
+
 
 ################################################################################
 # Build an i486-linux-musl cross compiler toolchain with the help of the
@@ -15,9 +29,17 @@ if [ ! -d i486-linux-musl ]; then
   cd musl-cross
   echo "ARCH=i486" > config.sh
   echo "CC_BASE_PREFIX=$BASEDIR" >> config.sh
+  if [ "x" != "x$ENABLE_DEBUG" ]; then
+    echo "MUSL_CONFFLAGS=--enable-debug" >> config.sh
+  fi
+
+  echo "MUSL_VERSION=master" >> config.sh
+  echo "MUSL_GIT=yes" >> config.sh
+
   ./build.sh
   cd ..
 fi
+
 
 ################################################################################
 # Build ncurses using the i486-linux-musl toolchain
@@ -41,16 +63,22 @@ if [ ! -d ncurses-build ]; then
   mkdir ncurses-build
   cd ncurses-build
 
+  if [ "x" != "x$ENABLE_DEBUG" ]; then
+    NCURSES_DEBUG_FLAGS=--with-debug
+  else
+    NCURSES_DEBUG_FLAGS=--without-debug
+  fi
+
   CC=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-gcc \
     CXX=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-g++ \
     AR=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-ar \
-    CFLAGS="-O2 -D_GNU_SOURCE" \
+    CFLAGS="$BASIC_CFLAGS -D_GNU_SOURCE" \
     ../$NCURSES_NAME/configure \
+        $NCURSES_DEBUG_FLAGS --without-profile \
         --without-cxx --without-cxx-binding \
         --without-ada --without-manpages \
         --without-progs --without-tests \
         --without-pkg-config --without-shared \
-        --without-debug --without-profile \
         --without-libtool --with-termlib \
         --without-gpm --without-dlsym \
         --without-sysmouse \
@@ -58,18 +86,12 @@ if [ ! -d ncurses-build ]; then
         --without-develop \
         --enable-const \
         --host=i486-pc-linux-gnu --target=i486-pc-linux-gnu \
-        --prefix=$BASEDIR/ncurses-prefix
+        --prefix=/usr
 
   make
   cd ..
 fi
 
-# install
-if [ ! -d ncurses-prefix ]; then
-  cd ncurses-build
-  make install
-  cd ..
-fi
 
 ################################################################################
 # Build libffi using the i486-linux-musl toolchain
@@ -95,22 +117,16 @@ if [ ! -d libffi-build ]; then
 
   CC=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-gcc \
     AR=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-ar \
-    CFLAGS="-O2" \
+    CFLAGS="$BASIC_CFLAGS" \
     ../$LIBFFI_NAME/configure \
         --disable-shared --enable-static \
         --host=i486-pc-linux-gnu --target=i486-pc-linux-gnu \
-        --prefix=$BASEDIR/libffi-prefix
+        --prefix=/usr
 
   make
   cd ..
 fi
 
-# install
-if [ ! -d libffi-prefix ]; then
-  cd libffi-build
-  make install
-  cd ..
-fi
 
 ################################################################################
 # Build libbfd 2.17 using the i486-linux-musl toolchain
@@ -134,21 +150,14 @@ if [ ! -d binutils-2.17-build ]; then
 
   CC=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-gcc \
     AR=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-ar \
-    CFLAGS=-O2 \
+    CFLAGS="$BASIC_CFLAGS" \
     ../binutils-2.17/configure \
         --disable-shared --enable-static \
         --disable-nls --disable-werror \
         --build=i686-pc-linux-gnu --host=i486-pc-linux-gnu --target=i486-pc-linux-gnu \
-        --prefix=$BASEDIR/binutils-2.17-prefix
+        --prefix=/usr
 
   make
-  cd ..
-fi
-
-# install
-if [ ! -d binutils-2.17-prefix ]; then
-  cd binutils-2.17-build
-  make install
   cd ..
 fi
 
@@ -168,21 +177,16 @@ fi
 if [ ! -d $CUNIT_NAME ]; then
   tar jxf $CUNIT_PACKAGE
   cd $CUNIT_NAME
+
   CC=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-gcc \
     AR=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-ar \
-    CFLAGS=-O2 \
+    CFLAGS="$BASIC_CFLAGS" \
     ./configure \
         --disable-shared --enable-static \
         --build=i686-pc-linux-gnu --host=i486-pc-linux-gnu \
-        --prefix=$BASEDIR/libcunit-prefix
-  make
-  cd ..
-fi
+        --prefix=/usr
 
-# install
-if [ ! -d libcunit-prefix ]; then
-  cd $CUNIT_NAME
-  make install
+  make
   cd ..
 fi
 
@@ -212,23 +216,17 @@ if [ ! -d $BINUTILS_NAME-build ]; then
 
   CC="$BASEDIR/i486-linux-musl/bin/i486-linux-musl-gcc -Wl,-Bstatic -static-libgcc" \
     AR=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-ar \
-    CFLAGS=-O2 \
+    CFLAGS="$BASIC_CFLAGS" \
     ../$BINUTILS_NAME/configure \
         --disable-shared --enable-static \
         --disable-nls --disable-werror \
         --build=i686-pc-linux-gnu --host=i486-pc-linux-gnu --target=i486-pc-linux-gnu \
-        --prefix=$BASEDIR/$BINUTILS_NAME-prefix
+        --prefix=/usr
 
   make
   cd ..
 fi
 
-# install
-if [ ! -d $BINUTILS_NAME-prefix ]; then
-  cd $BINUTILS_NAME-build
-  make install
-  cd ..
-fi
 
 ################################################################################
 # Build a plain standalone fbc, with an rtlib built with the i486-linux-musl
@@ -238,98 +236,54 @@ fi
 # find the headers from libs installed above, and everything that cannot easily
 # be built against musl libc must be disabled.
 
-if [ ! -d fbc-native ]; then
-  git clone git://github.com/freebasic/fbc.git fbc-native
+if [ ! -d fbc ]; then
+  git clone git://github.com/freebasic/fbc.git fbc
 fi
 
-cd fbc-native
-make compiler DISABLE_OBJINFO=1 ENABLE_STANDALONE=1
+if [ "x" != "x$ENABLE_DEBUG" ]; then
+  RTLIB_DEBUG_FLAGS="-DDEBUG"
+else
+  RTLIB_DEBUG_FLAGS=""
+fi
+
+cd fbc
+make compiler FBFLAGS="$BASIC_FBFLAGS" DISABLE_OBJINFO=1 ENABLE_STANDALONE=1
 make rtlib ENABLE_STANDALONE=1 \
   CC=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-gcc \
   AR=$BASEDIR/i486-linux-musl/bin/i486-linux-musl-ar \
-  CFLAGS="-O2 -D_GNU_SOURCE \
-          -I$BASEDIR/ncurses-prefix/include \
-          -I$BASEDIR/ncurses-prefix/include/ncurses \
-          -I$BASEDIR/libffi-prefix/lib/$LIBFFI_NAME/include \
+  CFLAGS="$BASIC_CFLAGS $RTLIB_DEBUG_FLAGS -D_GNU_SOURCE \
+          -I$BASEDIR/ncurses-build/include \
+          -I$BASEDIR/libffi-build/include \
           -DDISABLE_X11 -DDISABLE_GPM -DDISABLE_OPENGL \
           -DPTHREAD_MUTEX_RECURSIVE_NP=PTHREAD_MUTEX_RECURSIVE"
 cd ..
 
 # binutils
-mkdir -p fbc-native/bin/linux
-cp $BINUTILS_NAME-prefix/bin/as  fbc-static/bin/linux
-cp $BINUTILS_NAME-prefix/bin/ar  fbc-static/bin/linux
-cp $BINUTILS_NAME-prefix/bin/ld  fbc-static/bin/linux
+mkdir -p fbc/bin/linux
+cp $BINUTILS_NAME-build/gas/as-new   fbc/bin/linux/as
+cp $BINUTILS_NAME-build/binutils/ar  fbc/bin/linux/ar
+cp $BINUTILS_NAME-build/ld/ld-new    fbc/bin/linux/ld
 
 # target libraries
-cp i486-linux-musl/i486-linux-musl/lib/crt1.o                fbc-native/lib/linux
-cp i486-linux-musl/i486-linux-musl/lib/crti.o                fbc-native/lib/linux
-cp i486-linux-musl/i486-linux-musl/lib/crtn.o                fbc-native/lib/linux
-cp i486-linux-musl/i486-linux-musl/lib/libc.a                fbc-native/lib/linux
-cp i486-linux-musl/i486-linux-musl/lib/libdl.a               fbc-native/lib/linux
-cp i486-linux-musl/i486-linux-musl/lib/libm.a                fbc-native/lib/linux
-cp i486-linux-musl/i486-linux-musl/lib/libpthread.a          fbc-native/lib/linux
-cp i486-linux-musl/i486-linux-musl/lib/libsupc++.a           fbc-native/lib/linux
-cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/crtbegin.o  fbc-native/lib/linux
-cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/crtend.o    fbc-native/lib/linux
-cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/libgcc.a    fbc-native/lib/linux
-cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/libgcc_eh.a fbc-native/lib/linux
-cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/libgcov.a   fbc-native/lib/linux
-cp ncurses-prefix/lib/libncurses.a                           fbc-native/lib/linux
-cp ncurses-prefix/lib/libtinfo.a                             fbc-native/lib/linux
-cp libffi-prefix/lib/libffi.a                                fbc-native/lib/linux
-cp binutils-2.17-prefix/lib/libbfd.a                         fbc-native/lib/linux
-cp binutils-2.17-prefix/lib/libiberty.a                      fbc-native/lib/linux
-cp libcunit-prefix/lib/libcunit.a                            fbc-native/lib/linux
+cp i486-linux-musl/i486-linux-musl/lib/crt1.o                fbc/lib/linux
+cp i486-linux-musl/i486-linux-musl/lib/crti.o                fbc/lib/linux
+cp i486-linux-musl/i486-linux-musl/lib/crtn.o                fbc/lib/linux
+cp i486-linux-musl/i486-linux-musl/lib/libc.a                fbc/lib/linux
+cp i486-linux-musl/i486-linux-musl/lib/libdl.a               fbc/lib/linux
+cp i486-linux-musl/i486-linux-musl/lib/libm.a                fbc/lib/linux
+cp i486-linux-musl/i486-linux-musl/lib/libpthread.a          fbc/lib/linux
+cp i486-linux-musl/i486-linux-musl/lib/libsupc++.a           fbc/lib/linux
+cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/crtbegin.o  fbc/lib/linux
+cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/crtend.o    fbc/lib/linux
+cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/libgcc.a    fbc/lib/linux
+cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/libgcc_eh.a fbc/lib/linux
+cp i486-linux-musl/lib/gcc/i486-linux-musl/4.7.1/libgcov.a   fbc/lib/linux
+cp ncurses-build/lib/libncurses.a                            fbc/lib/linux
+cp ncurses-build/lib/libtinfo.a                              fbc/lib/linux
+cp libffi-build/.libs/libffi.a                               fbc/lib/linux
+cp binutils-2.17-build/bfd/libbfd.a                          fbc/lib/linux
+cp binutils-2.17-build/libiberty/libiberty.a                 fbc/lib/linux
+cp $CUNIT_NAME/CUnit/Sources/.libs/libcunit.a                fbc/lib/linux
 
-################################################################################
-# Build another standalone fbc, using the fbc-native setup from above. This fbc
-# and will itself be linked against against musl libc.
-
-if [ ! -d fbc-static ]; then
-  git clone git://github.com/freebasic/fbc.git fbc-static
-fi
-
-cd fbc-static
-make compiler ENABLE_STANDALONE=1 ENABLE_FBBFD=217 \
-  FBC=$BASEDIR/fbc-native/fbc-new FBLFLAGS="-static -l tinfo"
-cd ..
-
-# copy over the rtlib and other target libs
-mkdir -p fbc-static/lib/linux
-cp fbc-native/lib/linux/fbextra.x    fbc-static/lib/linux
-cp fbc-native/lib/linux/fbrt0.o      fbc-static/lib/linux
-cp fbc-native/lib/linux/libfb.a      fbc-static/lib/linux
-cp fbc-native/lib/linux/libfbmt.a    fbc-static/lib/linux
-cp fbc-native/lib/linux/crt1.o       fbc-static/lib/linux
-cp fbc-native/lib/linux/crti.o       fbc-static/lib/linux
-cp fbc-native/lib/linux/crtn.o       fbc-static/lib/linux
-cp fbc-native/lib/linux/libc.a       fbc-static/lib/linux
-cp fbc-native/lib/linux/libdl.a      fbc-static/lib/linux
-cp fbc-native/lib/linux/libm.a       fbc-static/lib/linux
-cp fbc-native/lib/linux/libpthread.a fbc-static/lib/linux
-cp fbc-native/lib/linux/libsupc++.a  fbc-static/lib/linux
-cp fbc-native/lib/linux/crtbegin.o   fbc-static/lib/linux
-cp fbc-native/lib/linux/crtend.o     fbc-static/lib/linux
-cp fbc-native/lib/linux/libgcc.a     fbc-static/lib/linux
-cp fbc-native/lib/linux/libgcc_eh.a  fbc-static/lib/linux
-cp fbc-native/lib/linux/libgcov.a    fbc-static/lib/linux
-cp fbc-native/lib/linux/libncurses.a fbc-static/lib/linux
-cp fbc-native/lib/linux/libtinfo.a   fbc-static/lib/linux
-cp fbc-native/lib/linux/libffi.a     fbc-static/lib/linux
-cp fbc-native/lib/linux/libbfd.a     fbc-static/lib/linux
-cp fbc-native/lib/linux/libiberty.a  fbc-static/lib/linux
-cp fbc-native/lib/linux/libcunit.a   fbc-static/lib/linux
-
-# copy in the static binutils
-mkdir -p fbc-static/bin/linux
-cp fbc-native/bin/linux/as  fbc-static/bin/linux
-cp fbc-native/bin/linux/ar  fbc-static/bin/linux
-cp fbc-native/bin/linux/ld  fbc-static/bin/linux
-
-################################################################################
-# package
-
-#cd libfb-linux-musl
-#tar zcf ../libfb-linux-musl.tar.gz *
-#cd ..
+# done
+$BASEDIR/fbc/fbc-new -static -l tinfo -version
