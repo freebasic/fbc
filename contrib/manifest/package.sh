ee2f18792d35f639553795710db3d78fbf6cf791
dkl <daniel.c.klauer@web.de>
2013-03-06 17:34:12 +0100

    package script: Determine .zip/.tar.gz based on release name

--- contrib/manifest/package.sh
+++ contrib/manifest/package.sh
@@ -3,24 +3,14 @@ set -ex
 
 show_usage()
 {
-  echo "usage: contrib/manifest/package.sh <release-name> <package-name> {targz|zip}"
-  echo " e.g.: contrib/manifest/package.sh linux FreeBASIC-0.24.0-linux targz"
+  echo "usage: contrib/manifest/package.sh <release-name> <package-name>"
+  echo " e.g.: contrib/manifest/package.sh linux FreeBASIC-0.24.0-linux"
 }
 
 if [ -z "$1" ] || [ -z "$2" ]; then
   show_usage
   exit 1
 fi
-
-case "$3" in
-zip|targz)
-	;;
-*)
-	show_usage
-	exit 1
-	;;
-esac
-
 
 # For non-standalone releases, copy the includes into the proper directory
 case "$1" in
@@ -35,11 +25,11 @@ djgpp)
 esac
 
 # Remove existing archive, if any
-case "$3" in
-zip)
+case "$1" in
+dos|djgpp|win32|mingw32)
 	rm -f $2.zip
 	;;
-targz)
+*)
 	rm $2.tar.gz
 	;;
 esac
@@ -48,12 +38,12 @@ esac
 find . -type f | cut -c3- | contrib/manifest/exclude "$1" | sort -f > "contrib/manifest/$1.lst"
 
 # Create an archive containing all the files listed in the manifest
-case "$3" in
-zip)
+case "$1" in
+dos|djgpp|win32|mingw32)
 	zip -q $2.zip -@ < contrib/manifest/$1.lst
 	;;
-targz)
-	tar -c -z -f $2.tar.gz -T contrib/manifest/$1.lst
+*)
+	tar -czf $2.tar.gz -T contrib/manifest/$1.lst
 	;;
 esac
 
