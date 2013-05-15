freebasic/fbc

commit b5129f76a3da23402a292b27db8fba063c37936b

Don't emit or destruct temp vars for sizeof() or typeof() argument
The expression given to sizeof() or typeof(), and also len() when evaluated
at compile-time, will be deleted and not be emitted. Thus we should not
emit any temp vars it used/created either, because they would just waste
stack space, and more importantly, we must not call destructors for such
temp vars either, because they were never constructed or even cleared,
since the expression isn't even emitted.

Otherwise we may end up calling a destructor of some uninitialized class
object, or fb_StrDelete() on an uninitialized FBSTRING, etc.
This crashed the test suite under -gen gcc due to a typeof( iif() ) where
the iif() allocated an FBSTRING temp var and fb_StrDelete() was called on
an uninitialized FBSTRING. The same problem existed for -gen gas of course,
although the test suite didn't happen to crash under -gen gas. But valgrind
showed the accesses to uninitialized memory.

This patch ...
- extracts the temporary scope handling code from ast-node-typeini.bas,
  and moves it into ast-node-scope.bas in form of several astTempScope*()
  functions
- adjusts the typeof() and sizeof()/len() parsers to use the temporary
  scopes when parsing their expressions so that any temporary symbols are
  captured and can be deleted.
- before, astDtorListClear() was called manually after cInitializer()
  calls, but as simplification the astTempScope*() code will take care of
  doing this now, in form of astDtorListDel() calls for each symbol added
  to the temporary scope.
- This results in much more fine-grained control over which entries are
  removed from the AST dtorlist, and that's useful for typeof()/sizeof(),
  because we only want to remove the dtor calls corresponding to their
  argument, but not those from any other surrounding expressions, as in:
      print foo + sizeof( bar )
  Here we want to drop the temp vars from bar, but we also want to handle

src/compiler/parser-decl-symbtype.bas
@@ -152,12 +152,20 @@ sub cTypeOf _
 	)
 
 	dim as ASTNODE ptr expr = any
+	dim as FBSYMBOL ptr scp = any, lastscp = any
+
+	'' Capture any temp vars, they shouldn't be emitted/allocated (and no
+	'' dtor calls for them either), since the expression will be deleted.
+	scp = astTempScopeBegin( lastscp, NULL )
 
 	'' Type or an Expression
 	expr = cTypeOrExpression( FALSE, dtype, subtype, lgt )
 
+	astTempScopeEnd( scp, lastscp )
+
 	'' Was it a type?
 	if( expr = NULL ) then
+		astTempScopeDelete( scp )
 		exit sub
 	end if
 
@@ -173,6 +181,7 @@ sub cTypeOf _
 	lgt     = astSizeOf( expr )
 
 	astDelTree( expr )
+	astTempScopeDelete( scp )
 end sub
 
 '':::::

src/compiler/ast-helper.bas
@@ -55,9 +55,11 @@ function astBuildFakeWstringAssign _
 		t = astNewLINK( t, astRemSideFx( expr ), FALSE )
 	end if
 
-	'' wcharptr = WstrAlloc( len( expr ) )
+	assert( astGetDataType( expr ) = FB_DATATYPE_WCHAR )
+
+	'' wcharptr = WstrAlloc( WstrLen( expr ) )
 	t = astNewLINK( t, _
-		astBuildVarAssign( sym, rtlWstrAlloc( rtlMathLen( astCloneTree( expr ) ) ) ), _
+		astBuildVarAssign( sym, rtlWstrAlloc( rtlWstrLen( astCloneTree( expr ) ) ) ), _
 		FALSE )
 
 	'' *wcharptr = expr

src/compiler/ast-misc.bas
@@ -1146,19 +1146,6 @@ function astDtorListFlush( byval cookie as integer ) as ASTNODE ptr
 	function = t
 end function
 
-sub astDtorListClear( )
-    dim as AST_DTORLIST_ITEM ptr n = any, p = any
-
-	n = listGetTail( @ast.dtorlist )
-	do while( n <> NULL )
-		p = listGetPrev( n )
-		listDelNode( @ast.dtorlist, n )
-		n = p
-    loop
-
-	ast.dtorlistcookies = 0  '' Can aswell be reset
-end sub
-
 sub astDtorListDel( byval sym as FBSYMBOL ptr )
     dim as AST_DTORLIST_ITEM ptr n = any
 

src/compiler/ast-node-scope.bas
@@ -24,12 +24,7 @@ declare sub hDelLocals _
 		byval check_backward as integer _
 	)
 
-'':::::
-function astScopeBegin _
-	( _
-		_
-	) as ASTNODE ptr
-
+function astScopeBegin( ) as ASTNODE ptr
     dim as ASTNODE ptr n = any
     dim as FBSYMBOL ptr s = any
 
@@ -70,15 +65,13 @@ function astScopeBegin _
 	astAdd( astNewDBG( AST_OP_DBG_SCOPEINI, cint( s ) ) )
 
 	function = n
-
 end function
 
-'':::::
 private sub hAddToBreakList _
 	( _
 		byval list as AST_BREAKLIST ptr, _
 		byval node as ASTNODE ptr _
-	) static
+	)
 
 	if( list->tail <> NULL ) then
 		list->tail->next = node
@@ -92,7 +85,7 @@ private sub hAddToBreakList _
 
 end sub
 
-sub astScopeBreak(byval target as FBSYMBOL ptr)
+sub astScopeBreak( byval target as FBSYMBOL ptr )
 	dim as ASTNODE ptr n = any
 
 	n = astNewNode( AST_NODECLASS_SCOPE_BREAK, FB_DATATYPE_INVALID, NULL )
@@ -113,12 +106,7 @@ sub astScopeBreak(byval target as FBSYMBOL ptr)
 	hAddToBreakList( @ast.proc.curr->block.breaklist, n )
 end sub
 
-'':::::
-sub astScopeEnd _
-	( _
-		byval n as ASTNODE ptr _
-	)
-
+sub astScopeEnd( byval n as ASTNODE ptr )
 	dim as FBSYMBOL ptr s = any
 
 	assert( n->class = AST_NODECLASS_SCOPEBEGIN )
@@ -153,15 +141,9 @@ sub astScopeEnd _
 	astAdd( n )
 
 	n->sym = s
-
 end sub
 
-'':::::
-function astScopeUpdBreakList _
-	( _
-		byval proc as ASTNODE ptr _
-	) as integer
-
+function astScopeUpdBreakList( byval proc as ASTNODE ptr ) as integer
     dim as ASTNODE ptr n = any
 
     function = FALSE
@@ -188,7 +170,6 @@ function astScopeUpdBreakList _
     loop
 
     function = TRUE
-
 end function
 
 '':::::
@@ -616,12 +597,14 @@ private function hCheckBranch _
 
 end function
 
-sub astScopeDestroyVars(byval symtbtail as FBSYMBOL ptr)
+sub astScopeDestroyVars( byval symtbtail as FBSYMBOL ptr )
+	dim as FBSYMBOL ptr s = any
+
 	'' For each symbol declared inside the block (in reverse order)
-	dim as FBSYMBOL ptr s = symtbtail
-	while (s)
+	s = symtbtail
+	while( s )
 		'' variable?
-		if (symbIsVar(s)) then
+		if( symbIsVar( s ) ) then
 			'' has a dtor?
 			if( symbGetVarHasDtor( s ) ) then
 				astAdd( astBuildVarDtorCall( s, TRUE ) )
@@ -687,12 +670,7 @@ sub astScopeAllocLocals( byval symtbhead as FBSYMBOL ptr )
 	end if
 end sub
 
-'':::::
-function astLoadSCOPEBEGIN _
-	( _
-		byval n as ASTNODE ptr _
-	) as IRVREG ptr
-
+function astLoadSCOPEBEGIN( byval n as ASTNODE ptr ) as IRVREG ptr
     dim as FBSYMBOL ptr s = any
 
 	s = n->sym
@@ -705,15 +683,9 @@ function astLoadSCOPEBEGIN _
 	end if
 
 	function = NULL
-
 end function
 
-'':::::
-function astLoadSCOPEEND _
-	( _
-		byval n as ASTNODE ptr _
-	) as IRVREG ptr
-
+function astLoadSCOPEEND( byval n as ASTNODE ptr ) as IRVREG ptr
     dim as FBSYMBOL ptr s = any
 
     s = n->sym
@@ -725,5 +697,99 @@ function astLoadSCOPEEND _
     symbSetProcLocalOfs( parser.currproc, s->scp.emit.baseofs )
 
     function = NULL
-
 end function
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'' Temporary scopes used by TYPEINIs to capture any temp symbols, so that they
+'' can be duplicated into other scope contexts
+'' (needed for field/parameter/global var initializer expressions)
+
+function astTempScopeBegin _
+	( _
+		byref lastscp as FBSYMBOL ptr, _
+		byval backnode as ASTNODE ptr _
+	) as FBSYMBOL ptr
+
+	dim as FBSYMBOL ptr scp = any
+
+	scp = symbAddScope( backnode )
+
+	lastscp = parser.currblock
+
+	parser.scope += 1
+	parser.currblock = scp
+
+	symbSetCurrentSymTb( @scp->scp.symtb )
+
+	function = scp
+end function
+
+sub astTempScopeEnd _
+	( _
+		byval scp as FBSYMBOL ptr, _
+		byval lastscp as FBSYMBOL ptr _
+	)
+
+	dim as FBSYMBOL ptr sym = any
+
+	'' remove symbols from hash table
+	symbDelScopeTb( scp )
+
+	'' back to previous symbol tb
+	symbSetCurrentSymTb( scp->symtb )
+
+	symbFreeSymbol_UnlinkOnly( scp )
+
+	parser.currblock = lastscp
+	parser.scope -= 1
+
+	'' Unregister any temp var dtor calls
+	'' (astTempScopeClone() will re-add them in the new context)
+	sym = symbGetScopeSymbTbHead( scp )
+	while( sym )
+		if( symbIsVar( sym ) ) then
+			astDtorListDel( sym )
+		end if
+		sym = sym->next
+	wend
+
+end sub
+
+sub astTempScopeClone _
+	( _
+		byval scp as FBSYMBOL ptr, _
+		byval clonetree as ASTNODE ptr _
+	)
+
+	dim as FBSYMBOL ptr sym = any, clonesym = any
+
+	'' Duplicate any symbols from the temp scope into the current context
+	sym = symbGetScopeSymbTbHead( scp )
+	while( sym )
+		clonesym = symbCloneSymbol( sym )
+
+		'' Update the corresponding expression tree
+		astReplaceSymbolOnTree( clonetree, sym, clonesym )
+
+		'' Re-register temp var dtors in the current context
+		if( symbIsVar( clonesym ) ) then
+			astDtorListAdd( clonesym )
+		end if
+
+		sym = sym->next
+	wend
+
+end sub
+
+sub astTempScopeDelete( byval scp as FBSYMBOL ptr )
+	dim as FBSYMBOL ptr sym = any, nxt = any
+
+	sym = symbGetScopeSymbTbHead( scp )
+	while( sym )
+		nxt = sym->next
+		symbFreeSymbol_RemOnly( sym )
+		sym = nxt
+	wend
+
+	symbFreeSymbol_RemOnly( scp )
+end sub

src/compiler/ast-node-typeini.bas
@@ -43,16 +43,7 @@ function astTypeIniBegin _
 	if( add_scope ) then
 		'' create a new scope block to handle temp vars allocated inside the
 		'' tree - with shared vars, the temps must be moved to another function
-    	dim as FBSYMBOL ptr s = symbAddScope( n )
-
-		n->typeini.scp = s
-		n->typeini.lastscp = parser.currblock
-
-		parser.scope += 1
-		parser.currblock = s
-
-		symbSetCurrentSymTb( @s->scp.symtb )
-
+		n->typeini.scp = astTempScopeBegin( n->typeini.lastscp, n )
 	else
 		n->typeini.scp = NULL
 	end if
@@ -68,6 +59,7 @@ sub astTypeIniEnd _
 
     dim as ASTNODE ptr n = any, p = any, l = any, r = any
     dim as integer ofs = any
+	dim as FBSYMBOL ptr sym = any
 
 	'' can't leave r pointing to the any node as the
 	'' tail node is linked already
@@ -120,16 +112,7 @@ sub astTypeIniEnd _
 
 	'' close the scope block
 	if( tree->typeini.scp <> NULL ) then
-		'' remove symbols from hash table
-		symbDelScopeTb( tree->typeini.scp )
-
-		'' back to preview symbol tb
-		symbSetCurrentSymTb( tree->typeini.scp->symtb )
-
-		symbFreeSymbol_UnlinkOnly( tree->typeini.scp )
-
-		parser.currblock = tree->typeini.lastscp
-		parser.scope -= 1
+		astTempScopeEnd( tree->typeini.scp, tree->typeini.lastscp )
 	end if
 
 end sub
@@ -995,29 +978,13 @@ end function
 '' original TYPEINI's temp scope are duplicated into the current scope context.
 function astTypeIniClone( byval tree as ASTNODE ptr ) as ASTNODE ptr
 	dim as ASTNODE ptr clonetree = any
-	dim as FBSYMBOL ptr sym = any, clonesym = any
 
 	assert( astIsTYPEINI( tree ) )
 
 	clonetree = astCloneTree( tree )
 
 	if( tree->typeini.scp ) then
-		sym = symbGetScopeSymbTbHead( tree->typeini.scp )
-		while( sym )
-			clonesym = symbCloneSymbol( sym )
-
-			astReplaceSymbolOnTree( clonetree, sym, clonesym )
-
-			'' Re-register temp var dtors
-			'' (They were removed via astDtorListClear() after
-			'' parsing the original expression, and must be re-added
-			'' everytime the initializer expression is instantiated)
-			if( symbIsVar( clonesym ) ) then
-				astDtorListAdd( clonesym )
-			end if
-
-			sym = sym->next
-		wend
+		astTempScopeClone( tree->typeini.scp, clonetree )
 	end if
 
 	function = clonetree
@@ -1055,20 +1022,10 @@ end function
 
 '' For deleting param/var/field initializer TYPEINIs and their temp scope
 sub astTypeIniDelete( byval tree as ASTNODE ptr )
-	dim as FBSYMBOL ptr sym = any, nxt = any
-
 	assert( astIsTYPEINI( tree ) )
 
 	if( tree->typeini.scp ) then
-		sym = symbGetScopeSymbTbHead( tree->typeini.scp )
-		while( sym )
-			nxt = sym->next
-			symbFreeSymbol_RemOnly( sym )
-			sym = nxt
-		wend
-
-		symbFreeSymbol_RemOnly( tree->typeini.scp )
-
+		astTempScopeDelete( tree->typeini.scp )
 		tree->typeini.scp = NULL
 	end if
 

src/compiler/ast.bi
@@ -476,25 +476,28 @@ declare sub astProcAddGlobalInstance _
 		byval has_dtor as integer _
 	)
 
-declare function astScopeBegin _
+declare function astScopeBegin( ) as ASTNODE ptr
+declare sub astScopeBreak( byval tolabel as FBSYMBOL ptr )
+declare sub astScopeEnd( byval s as ASTNODE ptr )
+declare function astScopeUpdBreakList( byval proc as ASTNODE ptr ) as integer
+declare sub astScopeDestroyVars( byval symtbtail as FBSYMBOL ptr )
+declare sub astScopeAllocLocals( byval symtbhead as FBSYMBOL ptr )
+declare function astTempScopeBegin _
 	( _
-		_
-	) as ASTNODE ptr
-
-declare sub astScopeEnd _
+		byref lastscp as FBSYMBOL ptr, _
+		byval backnode as ASTNODE ptr _
+	) as FBSYMBOL ptr
+declare sub astTempScopeEnd _
 	( _
-		byval s as ASTNODE ptr _
+		byval scp as FBSYMBOL ptr, _
+		byval lastscp as FBSYMBOL ptr _
 	)
-
-declare sub astScopeBreak(byval tolabel as FBSYMBOL ptr)
-
-declare function astScopeUpdBreakList _
+declare sub astTempScopeClone _
 	( _
-		byval proc as ASTNODE ptr _
-	) as integer
-
-declare sub astScopeDestroyVars(byval symtbtail as FBSYMBOL ptr)
-declare sub astScopeAllocLocals(byval symtbhead as FBSYMBOL ptr)
+		byval scp as FBSYMBOL ptr, _
+		byval clonetree as ASTNODE ptr _
+	)
+declare sub astTempScopeDelete( byval scp as FBSYMBOL ptr )
 
 declare function astAdd _
 	( _
@@ -1222,7 +1225,6 @@ declare sub astReplaceSymbolOnTree _
 
 declare sub astDtorListAdd( byval sym as FBSYMBOL ptr )
 declare function astDtorListFlush( byval cookie as integer = 0 ) as ASTNODE ptr
-declare sub astDtorListClear( )
 declare sub astDtorListDel( byval sym as FBSYMBOL ptr )
 declare sub astDtorListScopeBegin( )
 declare function astDtorListScopeEnd( ) as integer

src/compiler/parser-decl-proc-params.bas
@@ -156,8 +156,8 @@ private function hOptionalExpr _
 		exit function
 	end if
 
-	'' remove the temps from the dtors list if any was added
-	astDtorListClear( )
+	'' No temp dtors should be left registered after the TYPEINI build-up
+	assert( astDtorListIsEmpty( ) )
 
 	'' Disallow references to local vars, except for temp vars/descriptors
 	if( astTypeIniUsesLocals( expr, FB_SYMBATTRIB_TEMP or FB_SYMBATTRIB_DESCRIPTOR ) ) then

src/compiler/parser-decl-struct.bas
@@ -226,8 +226,8 @@ private sub hFieldInit( byval parent as FBSYMBOL ptr, byval sym as FBSYMBOL ptr 
 		end if
 	end if
 
-	'' remove the temps from the dtors list if any was added
-	astDtorListClear( )
+	'' No temp dtors should be left registered after the TYPEINI build-up
+	assert( astDtorListIsEmpty( ) )
 
 	'' Field initializers are only used in constructors (replacing the
 	'' implicit default initialization), so we make sure to add a default

commit c0325ad407fd8091d70f4ba0f79f20ff32a9ed6c

cTypeOf(): Fix dangling FBSYMBOL pointer
The astTempScopeBegin/End() code in cTypeOf() was intended to capture
temporary variables created while parsing the first operand of a typeof(),
in case it was a expression such as iif(). These temp var(s) were supposed
to be deleted -- afterall they will be unused anyways, as typeof()'s
operand is never emitted anyways.

However, this temporary scope also captured any other symbols, even when
the typeof() operand was a type (not an expression). Specifically, the
symbol returned as the subtype could be captured in the temporary scope,
in case the subtype symbol was newly created while parsing the typeof()
operand. cSymbolType() will sometimes create new symbols, especially for
function pointers.

Before cTypeOf() returns, it will do astTempScopeDelete(), which deletes
all symbols in the temporary scope. This could cause the subtype to
become a dangling pointer.

I noticed this problem while trying to implement support for
typeof( sub() ) or typeof( function() as integer ). Here cSymbolType()
will use symbAddProcPtr() to add a new PROC symbol to represent the
function pointer. It was captured in the temporary scope and deleted,
causing cTypeOf() to return a dangling pointer in the subtype which caused
strange behaviour in other parts of the compiler (false-positive warnings,
assert()ions triggered).

This problem was introduced with b5129f76 with the intention of
	1) removing dtor calls to temp vars for typeof/sizeof expressions
	2) completely deleting these temp vars, i.e. not emitting them
	   at all, which would just waste stack space

Then, 365c6d2b revisited the issue and adjusted the AST to handle the
dtor call problem in a different way. Apparently I forgot about point 2)
when working on 365c6d2b, because for some reason, it removes the
astTempScopeBegin/End() code from hLenSizeof() but not from cTypeOf().
That means temp vars in the sizeof() operand are emitted again, only the
dtor calls are still avoided.

src/compiler/ast.bas
@@ -247,6 +247,8 @@ function astCloneTree( byval n as ASTNODE ptr ) as ASTNODE ptr
 
 	select case( n->class )
 	case AST_NODECLASS_VAR
+		'' When cloning a temp VAR access, the AST dtorlist must be
+		'' told about the additional reference
 		if( c->sym ) then
 			if( symbIsVar( c->sym ) and symbIsTemp( c->sym ) ) then
 				astDtorListAddRef( c->sym )

src/compiler/parser-decl-symbtype.bas
@@ -148,20 +148,12 @@ sub cTypeOf _
 	)
 
 	dim as ASTNODE ptr expr = any
-	dim as FBSYMBOL ptr scp = any, lastscp = any
-
-	'' Capture any temp vars, they shouldn't be emitted/allocated (and no
-	'' dtor calls for them either), since the expression will be deleted.
-	scp = astTempScopeBegin( lastscp, NULL )
 
 	'' Type or an Expression
 	expr = cTypeOrExpression( FALSE, dtype, subtype, lgt )
 
-	astTempScopeEnd( scp, lastscp )
-
 	'' Was it a type?
 	if( expr = NULL ) then
-		astTempScopeDelete( scp )
 		exit sub
 	end if
 
@@ -177,7 +169,6 @@ sub cTypeOf _
 	lgt     = astSizeOf( expr )
 
 	astDelTree( expr )
-	astTempScopeDelete( scp )
 end sub
 
 function hIntegerTypeFromBitSize _