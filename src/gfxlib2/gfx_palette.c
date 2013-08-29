a185d539907b8bee21f71b32e1250b7d6655e0d6
dkl <daniel.c.klauer@web.de>
2013-08-29 21:04:08 +0200

    gfxlib2: Split fb_GfxPaletteUsing() into separate module
    fb_GfxPalette() is always included because it's used from gfx_screen.c,
    but fb_GfxPaletteUsing() can be in a separate module so that it's only
    included when actually used.

--- src/gfxlib2/gfx_palette.c
+++ src/gfxlib2/gfx_palette.c
@@ -132,22 +132,3 @@ FBCALL void fb_GfxPalette(int index, int red, int green, int blue)
 
 	DRIVER_UNLOCK();
 }
-
-FBCALL void fb_GfxPaletteUsing(int *data)
-{
-	int i;
-
-	if ((!__fb_gfx) || (__fb_gfx->depth > 8))
-		return;
-
-	DRIVER_LOCK();
-
-	for (i = 0; i < (1 << __fb_gfx->depth); i++) {
-		if (data[i] >= 0)
-			fb_hSetPaletteColor(i, data[i]);
-	}
-
-	fb_hMemSet(__fb_gfx->dirty, TRUE, __fb_gfx->h);
-
-	DRIVER_UNLOCK();
-}
