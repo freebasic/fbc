b7858ca20bfcdcb9b533ad7dc796fe849cf5c5e1
dkl <daniel.c.klauer@web.de>
2013-10-07 19:12:18 +0200

    rtlib: Fix & simplify fb_CrtFileCopy()
    Instead of doing fseek/ftell/fseek to find out the file size, and then
    copy that many bytes, the new version simply copies everything until EOF
    is encountered, that's easier and faster.
    
    Besides that, the old code stored the file size in an int and thus was
    limited to 32bit, and couldn't handle larger files.

--- src/rtlib/file_copy_crt.c
+++ src/rtlib/file_copy_crt.c
@@ -2,42 +2,30 @@
 
 #include "fb.h"
 
-#define FILE_COPY_BUF_SIZE 512
+#define BUFFER_SIZE 512
 
 FBCALL int fb_CrtFileCopy( const char *source, const char *destination )
 {
-	FILE *src, *dst = NULL;
-	int len;
-	size_t bytes_to_copy;
-	char buf[FILE_COPY_BUF_SIZE];
+	FILE *src, *dst;
+	unsigned char buffer[BUFFER_SIZE];
+	size_t bytesread;
 
+	dst = NULL;
 	src = fopen(source, "rb");
-
 	if (!src)
 		goto err;
-
-	fseek(src, 0, SEEK_END);
-	len = ftell(src);
-	fseek(src, 0, SEEK_SET);
 
 	dst = fopen(destination, "wb");
 	if (!dst)
 		goto err;
-	
-	while (len > 0) {
-		if (len >= FILE_COPY_BUF_SIZE)
-			bytes_to_copy = FILE_COPY_BUF_SIZE;
-		else
-			bytes_to_copy = len;
-		
-		if (fread( buf, 1, bytes_to_copy, src ) != bytes_to_copy)
+
+	while( (bytesread = fread( buffer, 1, BUFFER_SIZE, src )) > 0 ) {
+		if (fwrite( buffer, 1, bytesread, dst ) != bytesread)
 			goto err;
-		
-		if (fwrite( buf, 1, bytes_to_copy, dst ) != bytes_to_copy)
-			goto err;
-		
-		len -= bytes_to_copy;
 	}
+
+	if( !feof( src ) )
+		goto err;
 
 	fclose( src );
 	fclose( dst );
