18242bc06199f069a70c1f5bf33b1f3042f1d70c
dkl <daniel.c.klauer@web.de>
2013-10-17 00:16:05 +0200

    rtlib: Re-use LongInt Bin/Oct/Hex functions to implement 32bit ones
    This basically makes the following commit easier to implement, by
    removing some duplicate code.

--- src/rtlib/str_bin.c
+++ src/rtlib/str_bin.c
@@ -2,94 +2,32 @@
 
 #include "fb.h"
 
-static FBSTRING *hBIN( unsigned int num, ssize_t len, int digits )
-{
-	FBSTRING *dst;
-	char *buf;
-	int	i, totdigs;
-	unsigned int mask;
-
-	if( digits > 0 )
-	{
-		totdigs = (digits < len << 3? digits: len << 3);
-		if( digits > len << 3 )
-			digits = len << 3;
-	}
-	else
-		totdigs = len << 3;
-
-	/* alloc temp string */
-    dst = fb_hStrAllocTemp( NULL, totdigs );
-	if( dst == NULL )
-		return &__fb_ctx.null_desc;
-
-	/* convert */
-	buf = dst->data;
-
-	if( num == 0 )
-	{
-		if( digits <= 0 )
-			digits = 1;
-
-		while( digits-- )
-			*buf++ = '0';
-	}
-	else
-	{
-		mask = 1UL << (totdigs-1);
-
-		for( i = 0; i < totdigs; i++, num <<= 1 )
-			if( num & mask )
-				break;
-
-		if( digits > 0 )
-		{
-			digits -= totdigs - i;
-			while( digits-- )
-				*buf++ = '0';
-		}
-
-		for( ; i < totdigs; i++, num <<= 1 )
-			if( num & mask )
-				*buf++ = '1';
-			else
-				*buf++ = '0';
-	}
-
-	/* add null-term */
-	*buf = '\0';
-
-    fb_hStrSetLength( dst, buf - dst->data );
-
-	return dst;
-}
-
 FBCALL FBSTRING *fb_BIN_b ( unsigned char num )
 {
-	return hBIN( num, sizeof( char ), 0 );
+	return fb_BINEx_l( num, 0 );
 }
 
 FBCALL FBSTRING *fb_BIN_s ( unsigned short num )
 {
-	return hBIN( num, sizeof( short ), 0 );
+	return fb_BINEx_l( num, 0 );
 }
 
 FBCALL FBSTRING *fb_BIN_i ( unsigned int num )
 {
-	return hBIN( num, sizeof( int ), 0 );
+	return fb_BINEx_l( num, 0 );
 }
 
 FBCALL FBSTRING *fb_BINEx_b( unsigned char num, int digits )
 {
-	return hBIN( num, sizeof( unsigned char ), digits );
+	return fb_BINEx_l( num, digits );
 }
 
 FBCALL FBSTRING *fb_BINEx_s( unsigned short num, int digits )
 {
-	return hBIN( num, sizeof( unsigned short ), digits );
+	return fb_BINEx_l( num, digits );
 }
 
 FBCALL FBSTRING *fb_BINEx_i ( unsigned int num, int digits )
 {
-	return hBIN( num, sizeof( int ), digits );
+	return fb_BINEx_l( num, digits );
 }
--- src/rtlib/str_hex.c
+++ src/rtlib/str_hex.c
@@ -2,92 +2,32 @@
 
 #include "fb.h"
 
-static char hex_table[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-
-static FBSTRING *hHEX( unsigned int num, ssize_t len, int digits )
-{
-	FBSTRING *dst;
-	char *buf;
-	int i, totdigs;
-
-	if( digits > 0 )
-	{
-		totdigs = (digits < len << 1? digits: len << 1);
-		if( digits > len << 1 )
-			digits = len << 1;
-	}
-	else
-		totdigs = len << 1;
-
-	/* alloc temp string */
-    dst = fb_hStrAllocTemp( NULL, totdigs );
-	if( dst == NULL )
-		return &__fb_ctx.null_desc;
-
-	/* convert */
-	buf = dst->data;
-
-	if( num == 0 )
-	{
-		if( digits <= 0 )
-			digits = 1;
-
-		while( digits-- )
-			*buf++ = '0';
-    }
-	else
-	{
-		num <<= ((sizeof(int) << 3) - (totdigs << 2));
-
-		for( i = 0; i < totdigs; i++, num <<= 4 )
-			if( num > 0x0FFFFFFF )
-				break;
-
-		if( digits > 0 )
-		{
-			digits -= totdigs - i;
-			while( digits-- )
-				*buf++ = '0';
-		}
-
-		for( ; i < totdigs; i++, num <<= 4 )
-			*buf++ = hex_table[(num & 0xF0000000) >> 28];
-	}
-
-	/* add null-term */
-	*buf = '\0';
-
-	fb_hStrSetLength( dst, buf - dst->data );
-
-	return dst;
-}
-
 FBCALL FBSTRING *fb_HEX_b ( unsigned char num )
 {
-	return hHEX( num, sizeof( char ), 0 );
+	return fb_HEXEx_l( num, 0 );
 }
 
 FBCALL FBSTRING *fb_HEX_s ( unsigned short num )
 {
-	return hHEX( num, sizeof( short ), 0 );
+	return fb_HEXEx_l( num, 0 );
 }
 
 FBCALL FBSTRING *fb_HEX_i ( unsigned int num )
 {
-	return hHEX( num, sizeof( int ), 0 );
+	return fb_HEXEx_l( num, 0 );
 }
 
 FBCALL FBSTRING *fb_HEXEx_b( unsigned char num, int digits )
 {
-	return hHEX( num, sizeof( unsigned char ), digits );
+	return fb_HEXEx_l( num, digits );
 }
 
 FBCALL FBSTRING *fb_HEXEx_s( unsigned short num, int digits )
 {
-	return hHEX( num, sizeof( unsigned short ), digits );
+	return fb_HEXEx_l( num, digits );
 }
 
 FBCALL FBSTRING *fb_HEXEx_i ( unsigned int num, int digits )
 {
-	return hHEX( num, sizeof( int ), digits );
+	return fb_HEXEx_l( num, digits );
 }
