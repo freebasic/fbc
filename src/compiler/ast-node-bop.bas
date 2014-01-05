freebasic/fbc

commit ea511505dec6223d6e767e099334be9a8e7d1d44

ast: Change FBVALUE & co to store all integer constants as 64bit

src/compiler/ast-node-bop.bas
@@ -232,338 +232,155 @@ private sub hToStr(byref l as ASTNODE ptr, byref r as ASTNODE ptr)
    	end select
 end sub
 
-private function hBOPConstFoldInt _
+private function hConstBop _
 	( _
 		byval op as integer, _
+		byval dtype as integer, _
+		byval subtype as FBSYMBOL ptr, _
 		byval l as ASTNODE ptr, _
 		byval r as ASTNODE ptr _
 	) as ASTNODE ptr
 
-	dim as integer issigned = any, ldfull = any
-	dim as FBSYMBOL ptr lsubtype = any
-
-	issigned = typeIsSigned( l->dtype )
-
-	select case as const op
-	case AST_OP_ADD
-		l->con.val.int = l->con.val.int + r->con.val.int
-
-	case AST_OP_SUB
-		l->con.val.int = l->con.val.int - r->con.val.int
-
-	case AST_OP_MUL
-		if( issigned ) then
-			l->con.val.int = l->con.val.int * r->con.val.int
-		else
-			l->con.val.int = cunsg(l->con.val.int) * cunsg(r->con.val.int)
-		end if
-
-	case AST_OP_INTDIV
-		if( r->con.val.int <> 0 ) then
-			if( issigned ) then
-				l->con.val.int = l->con.val.int \ r->con.val.int
+	if( typeGetClass( l->dtype ) = FB_DATACLASS_FPOINT ) then
+		select case as const( op )
+		case AST_OP_ADD
+			l->val.f = l->val.f + r->val.f
+		case AST_OP_SUB
+			l->val.f = l->val.f - r->val.f
+		case AST_OP_MUL
+			l->val.f = l->val.f * r->val.f
+		case AST_OP_DIV
+			'' Note: no division by zero error here - we should return
+			'' INF instead, just like with (l / r) at runtime
+			l->val.f = l->val.f / r->val.f
+		case AST_OP_POW
+			l->val.f = l->val.f ^ r->val.f
+		case AST_OP_NE
+			l->val.i = l->val.f <> r->val.f
+		case AST_OP_EQ
+			l->val.i = l->val.f = r->val.f
+		case AST_OP_GT
+			l->val.i = l->val.f > r->val.f
+		case AST_OP_LT
+			l->val.i = l->val.f < r->val.f
+		case AST_OP_LE
+			l->val.i = l->val.f <= r->val.f
+		case AST_OP_GE
+			l->val.i = l->val.f >= r->val.f
+		case AST_OP_ATAN2
+			l->val.f = atan2( l->val.f, r->val.f )
+		case AST_OP_ANDALSO
+			if l->val.f then
+				l->val.i = (r->val.f <> 0)
 			else
-				l->con.val.int = cunsg( l->con.val.int ) \ cunsg( r->con.val.int )
+				l->val.i = 0
 			end if
-		else
-			l->con.val.int = 0
-			errReport( FB_ERRMSG_DIVBYZERO )
-		end if
-
-	case AST_OP_MOD
-		if( r->con.val.int <> 0 ) then
-			if( issigned ) then
-				l->con.val.int = l->con.val.int mod r->con.val.int
+		case AST_OP_ORELSE
+			if l->val.f then
+				l->val.i = -1
 			else
-				l->con.val.int = cunsg( l->con.val.int ) mod cunsg( r->con.val.int )
+				l->val.i = (r->val.f <> 0)
 			end if
-		else
-			l->con.val.int = 0
-			errReport( FB_ERRMSG_DIVBYZERO )
-		end if
+		case else
+			assert( FALSE )
+		end select
 
-	case AST_OP_SHL
-		if( issigned ) then
-			l->con.val.int = l->con.val.int shl r->con.val.int
-		else
-			l->con.val.int = cunsg( l->con.val.int ) shl r->con.val.int
-		end if
+	elseif( typeIsSigned( l->dtype ) ) then
+		select case as const( op )
+		case AST_OP_ADD : l->val.i = l->val.i +   r->val.i
+		case AST_OP_SUB : l->val.i = l->val.i -   r->val.i
+		case AST_OP_MUL : l->val.i = l->val.i *   r->val.i
+		case AST_OP_SHL : l->val.i = l->val.i shl r->val.i
+		case AST_OP_SHR : l->val.i = l->val.i shr r->val.i
+		case AST_OP_AND : l->val.i = l->val.i and r->val.i
+		case AST_OP_OR  : l->val.i = l->val.i or  r->val.i
+		case AST_OP_XOR : l->val.i = l->val.i xor r->val.i
+		case AST_OP_EQV : l->val.i = l->val.i eqv r->val.i
+		case AST_OP_IMP : l->val.i = l->val.i imp r->val.i
+		case AST_OP_NE  : l->val.i = l->val.i <>  r->val.i
+		case AST_OP_EQ  : l->val.i = l->val.i =   r->val.i
+		case AST_OP_GT  : l->val.i = l->val.i >   r->val.i
+		case AST_OP_LT  : l->val.i = l->val.i <   r->val.i
+		case AST_OP_LE  : l->val.i = l->val.i <=  r->val.i
+		case AST_OP_GE  : l->val.i = l->val.i >=  r->val.i
+		case AST_OP_ANDALSO : l->val.i = iif( l->val.i <> 0, r->val.i <> 0, 0 )
+		case AST_OP_ORELSE  : l->val.i = iif( l->val.i <> 0, -1, r->val.i <> 0 )
+		case AST_OP_INTDIV
+			if( r->val.i <> 0 ) then
+				l->val.i = l->val.i \ r->val.i
+			else
+				l->val.i = 0
+				errReport( FB_ERRMSG_DIVBYZERO )
+			end if
+		case AST_OP_MOD
+			if( r->val.i <> 0 ) then
+				l->val.i = l->val.i mod r->val.i
+			else
+				l->val.i = 0
+				errReport( FB_ERRMSG_DIVBYZERO )
+			end if
+		case else
+			assert( FALSE )
+		end select
 
-	case AST_OP_SHR
-		if( issigned ) then
-			l->con.val.int = l->con.val.int shr r->con.val.int
-		else
-			l->con.val.int = cunsg( l->con.val.int ) shr r->con.val.int
-		end if
+		'' Pretend the CONST is a 64bit value for a moment, since the
+		'' result was calculated and stored at 64bit precision above,
+		'' then do a CONV back to the original type and let it show any
+		'' overflow warnings in case the real type cannot hold the
+		'' calculated value.
+		l->dtype = FB_DATATYPE_LONGINT
+		l->subtype = NULL
 
-	case AST_OP_AND
-		l->con.val.int = l->con.val.int and r->con.val.int
+		l = astNewCONV( dtype, subtype, l, AST_CONVOPT_DONTCHKPTR )
 
-	case AST_OP_OR
-		l->con.val.int = l->con.val.int or r->con.val.int
+	else
+		select case as const( op )
+		case AST_OP_ADD : l->val.i = cunsg( l->val.i ) +   cunsg( r->val.i )
+		case AST_OP_SUB : l->val.i = cunsg( l->val.i ) -   cunsg( r->val.i )
+		case AST_OP_MUL : l->val.i = cunsg( l->val.i ) *   cunsg( r->val.i )
+		case AST_OP_SHL : l->val.i = cunsg( l->val.i ) shl cunsg( r->val.i )
+		case AST_OP_SHR : l->val.i = cunsg( l->val.i ) shr cunsg( r->val.i )
+		case AST_OP_AND : l->val.i = cunsg( l->val.i ) and cunsg( r->val.i )
+		case AST_OP_OR  : l->val.i = cunsg( l->val.i ) or  cunsg( r->val.i )
+		case AST_OP_XOR : l->val.i = cunsg( l->val.i ) xor cunsg( r->val.i )
+		case AST_OP_EQV : l->val.i = cunsg( l->val.i ) eqv cunsg( r->val.i )
+		case AST_OP_IMP : l->val.i = cunsg( l->val.i ) imp cunsg( r->val.i )
+		case AST_OP_NE  : l->val.i = cunsg( l->val.i ) <>  cunsg( r->val.i )
+		case AST_OP_EQ  : l->val.i = cunsg( l->val.i ) =   cunsg( r->val.i )
+		case AST_OP_GT  : l->val.i = cunsg( l->val.i ) >   cunsg( r->val.i )
+		case AST_OP_LT  : l->val.i = cunsg( l->val.i ) <   cunsg( r->val.i )
+		case AST_OP_LE  : l->val.i = cunsg( l->val.i ) <=  cunsg( r->val.i )
+		case AST_OP_GE  : l->val.i = cunsg( l->val.i ) >=  cunsg( r->val.i )
+		case AST_OP_ANDALSO : l->val.i = iif( l->val.i <> 0, r->val.i <> 0, 0 )
+		case AST_OP_ORELSE  : l->val.i = iif( l->val.i <> 0, -1, r->val.i <> 0 )
+		case AST_OP_INTDIV
+			if( r->val.i <> 0 ) then
+				l->val.i = cunsg( l->val.i ) \ cunsg( r->val.i )
+			else
+				l->val.i = 0
+				errReport( FB_ERRMSG_DIVBYZERO )
+			end if
+		case AST_OP_MOD
+			if( r->val.i <> 0 ) then
+				l->val.i = cunsg( l->val.i ) mod cunsg( r->val.i )
+			else
+				l->val.i = 0
+				errReport( FB_ERRMSG_DIVBYZERO )
+			end if
+		case else
+			assert( FALSE )
+		end select
 
-	case AST_OP_ANDALSO
-		if l->con.val.int then
-			l->con.val.int = (r->con.val.int <> 0)
-		else
-			l->con.val.int = 0
-		end if
+		'' (ditto)
+		l->dtype = FB_DATATYPE_ULONGINT
+		l->subtype = NULL
 
-	case AST_OP_ORELSE
-		if l->con.val.int then
-			l->con.val.int = -1
-		else
-			l->con.val.int = (r->con.val.int <> 0)
-		end if
+		l = astNewCONV( dtype, subtype, l, AST_CONVOPT_DONTCHKPTR )
 
-	case AST_OP_XOR
-		l->con.val.int = l->con.val.int xor r->con.val.int
+	end if
 
-	case AST_OP_EQV
-		l->con.val.int = l->con.val.int eqv r->con.val.int
-
-	case AST_OP_IMP
-		l->con.val.int = l->con.val.int imp r->con.val.int
-
-	case AST_OP_NE
-		l->con.val.int = l->con.val.int <> r->con.val.int
-
-	case AST_OP_EQ
-		l->con.val.int = l->con.val.int = r->con.val.int
-
-	case AST_OP_GT
-		if( issigned ) then
-			l->con.val.int = l->con.val.int > r->con.val.int
-		else
-			l->con.val.int = cunsg( l->con.val.int ) > cunsg( r->con.val.int )
-		end if
-
-	case AST_OP_LT
-		if( issigned ) then
-			l->con.val.int = l->con.val.int < r->con.val.int
-		else
-			l->con.val.int = cunsg( l->con.val.int ) < cunsg( r->con.val.int )
-		end if
-
-	case AST_OP_LE
-		if( issigned ) then
-			l->con.val.int = l->con.val.int <= r->con.val.int
-		else
-			l->con.val.int = cunsg( l->con.val.int ) <= cunsg( r->con.val.int )
-		end if
-
-	case AST_OP_GE
-		if( issigned ) then
-			l->con.val.int = l->con.val.int >= r->con.val.int
-		else
-			l->con.val.int = cunsg( l->con.val.int ) >= cunsg( r->con.val.int )
-		end if
-	end select
-
-	'' Pretend the CONST is an integer for a moment, since the result was
-	'' calculated and stored at INTEGER precision above, then do a CONV
-	'' back to the original type and let it show any overflow warnings in
-	'' case the real type cannot hold the calculated value.
-	ldfull = l->dtype
-	lsubtype = l->subtype
-	l->dtype = FB_DATATYPE_INTEGER
-	l->subtype = NULL
-
-	function = astNewCONV( ldfull, lsubtype, l )
+	function = l
 end function
-
-'':::::
-private sub hBOPConstFoldFlt _
-	( _
-		byval op as integer, _
-		byval l as ASTNODE ptr, _
-		byval r as ASTNODE ptr _
-	) static
-
-	select case as const op
-	case AST_OP_ADD
-		l->con.val.float = l->con.val.float + r->con.val.float
-
-	case AST_OP_SUB
-		l->con.val.float = l->con.val.float - r->con.val.float
-
-	case AST_OP_MUL
-		l->con.val.float = l->con.val.float * r->con.val.float
-
-	case AST_OP_DIV
-		'' Note: no division by zero error here - we should return
-		'' INF instead, just like with (l / r) at runtime
-		l->con.val.float = l->con.val.float / r->con.val.float
-
-    case AST_OP_POW
-		l->con.val.float = l->con.val.float ^ r->con.val.float
-
-	case AST_OP_NE
-		l->con.val.int = l->con.val.float <> r->con.val.float
-
-	case AST_OP_EQ
-		l->con.val.int = l->con.val.float = r->con.val.float
-
-	case AST_OP_GT
-		l->con.val.int = l->con.val.float > r->con.val.float
-
-	case AST_OP_LT
-		l->con.val.int = l->con.val.float < r->con.val.float
-
-	case AST_OP_LE
-		l->con.val.int = l->con.val.float <= r->con.val.float
-
-	case AST_OP_GE
-		l->con.val.int = l->con.val.float >= r->con.val.float
-
-    case AST_OP_ATAN2
-		l->con.val.float = atan2( l->con.val.float, r->con.val.float )
-
-	case AST_OP_ANDALSO
-		if l->con.val.float then
-			l->con.val.int = (r->con.val.float <> 0)
-		else
-			l->con.val.int = 0
-		end if
-
-	case AST_OP_ORELSE
-		if l->con.val.float then
-			l->con.val.int = -1
-		else
-			l->con.val.int = (r->con.val.float <> 0)
-		end if
-	end select
-
-end sub
-
-private sub hBOPConstFold64 _
-	( _
-		byval op as integer, _
-		byval l as ASTNODE ptr, _
-		byval r as ASTNODE ptr _
-	)
-
-	dim as integer issigned = any
-
-	issigned = typeIsSigned( l->dtype )
-
-	select case as const op
-	case AST_OP_ADD
-		l->con.val.long = l->con.val.long + r->con.val.long
-
-	case AST_OP_SUB
-		l->con.val.long = l->con.val.long - r->con.val.long
-
-	case AST_OP_MUL
-		if( issigned ) then
-			l->con.val.long = l->con.val.long * r->con.val.long
-		else
-			l->con.val.long = cunsg(l->con.val.long) * cunsg(r->con.val.long)
-		end if
-
-	case AST_OP_INTDIV
-		if( r->con.val.long <> 0 ) then
-			if( issigned ) then
-				l->con.val.long = l->con.val.long \ r->con.val.long
-			else
-				l->con.val.long = cunsg( l->con.val.long ) \ cunsg( r->con.val.long )
-			end if
-		else
-			l->con.val.long = 0
-			errReport( FB_ERRMSG_DIVBYZERO )
-		end if
-
-	case AST_OP_MOD
-		if( r->con.val.long <> 0 ) then
-			if( issigned ) then
-				l->con.val.long = l->con.val.long mod r->con.val.long
-			else
-				l->con.val.long = cunsg( l->con.val.long ) mod cunsg( r->con.val.long )
-			end if
-		else
-			l->con.val.long = 0
-			errReport( FB_ERRMSG_DIVBYZERO )
-		end if
-
-	case AST_OP_SHL
-		if( issigned ) then
-			l->con.val.long = l->con.val.long shl r->con.val.int
-		else
-			l->con.val.long = cunsg( l->con.val.long ) shl r->con.val.int
-		end if
-
-	case AST_OP_SHR
-		if( issigned ) then
-			l->con.val.long = l->con.val.long shr r->con.val.int
-		else
-			l->con.val.long = cunsg( l->con.val.long ) shr r->con.val.int
-		end if
-
-	case AST_OP_AND
-		l->con.val.long = l->con.val.long and r->con.val.long
-
-	case AST_OP_OR
-		l->con.val.long = l->con.val.long or r->con.val.long
-
-	case AST_OP_ANDALSO
-		if l->con.val.long then
-			l->con.val.long = (r->con.val.long <> 0)
-		else
-			l->con.val.long = 0
-		end if
-
-	case AST_OP_ORELSE
-		if l->con.val.long then
-			l->con.val.long = -1
-		else
-			l->con.val.long = (r->con.val.long <> 0)
-		end if
-
-	case AST_OP_XOR
-		l->con.val.long = l->con.val.long xor r->con.val.long
-
-	case AST_OP_EQV
-		l->con.val.long = l->con.val.long eqv r->con.val.long
-
-	case AST_OP_IMP
-		l->con.val.long = l->con.val.long imp r->con.val.long
-
-	case AST_OP_NE
-		l->con.val.int = l->con.val.long <> r->con.val.long
-
-	case AST_OP_EQ
-		l->con.val.int = l->con.val.long = r->con.val.long
-
-	case AST_OP_GT
-		if( issigned ) then
-			l->con.val.int = l->con.val.long > r->con.val.long
-		else
-			l->con.val.int = cunsg( l->con.val.long ) > cunsg( r->con.val.long )
-		end if
-
-	case AST_OP_LT
-		if( issigned ) then
-			l->con.val.int = l->con.val.long < r->con.val.long
-		else
-			l->con.val.int = cunsg( l->con.val.long ) < cunsg( r->con.val.long )
-		end if
-
-	case AST_OP_LE
-		if( issigned ) then
-			l->con.val.int = l->con.val.long <= r->con.val.long
-		else
-			l->con.val.int = cunsg( l->con.val.long ) <= cunsg( r->con.val.long )
-		end if
-
-	case AST_OP_GE
-		if( issigned ) then
-			l->con.val.int = l->con.val.long >= r->con.val.long
-		else
-			l->con.val.int = cunsg( l->con.val.long ) >= cunsg( r->con.val.long )
-		end if
-	end select
-
-end sub
 
 '':::::
 private function hCheckPointer _
@@ -1307,8 +1124,8 @@ function astNewBOP _
 		if( astIsCONST( r ) ) then
 			'' warn if shift is greater than or equal to the number of bits in ldtype
 			'' !!!FIXME!!! prevent asm error when value is higher than 255
-			select case astGetValueAsLongInt( r )
-			case 0 to (typeGetSize( ldtype ) * 8)-1
+			select case astConstGetAsInt64( r )
+			case 0 to typeGetBits( ldtype )-1
 
 			case else
 				errReportWarn( FB_WARNINGMSG_SHIFTEXCEEDSBITSINDATATYPE )
@@ -1328,25 +1145,7 @@ function astNewBOP _
 
 	'' constant folding (won't handle commutation, ie: "1+a+2+3" will become "1+a+5", not "a+6")
 	if( astIsCONST( l ) and astIsCONST( r ) ) then
-
-		select case as const typeGet( ldtype )
-		case FB_DATATYPE_LONGINT, FB_DATATYPE_ULONGINT
-		    hBOPConstFold64( op, l, r )
-
-		case FB_DATATYPE_SINGLE, FB_DATATYPE_DOUBLE
-			hBOPConstFoldFlt( op, l, r )
-
-		case FB_DATATYPE_LONG, FB_DATATYPE_ULONG
-			if( FB_LONGSIZE = len( integer ) ) then
-				l = hBOPConstFoldInt( op, l, r )
-			else
-				hBOPConstFold64( op, l, r )
-			end if
-
-		case else
-			'' byte's, short's, int's and enum's
-			l = hBOPConstFoldInt( op, l, r )
-		end select
+		l = hConstBop( op, dtype, subtype, l, r )
 
 		astGetFullType( l ) = dtype
 		l->subtype = subtype
@@ -1381,7 +1180,7 @@ function astNewBOP _
 				'' no need to check for other values, floats aren't
 				'' allowed and if longints were used, this wouldn't be
 				'' an ofs node
-				l->ofs.ofs += r->con.val.int
+				l->ofs.ofs += r->val.i
 				astDelNode( r )
 
 				return l
@@ -1391,7 +1190,7 @@ function astNewBOP _
 			'' offset?
 			if( l->class = AST_NODECLASS_OFFSET ) then
 				'' see above
-				l->ofs.ofs -= r->con.val.int
+				l->ofs.ofs -= r->val.i
 				astDelNode( r )
 
 				return l
@@ -1404,15 +1203,13 @@ function astNewBOP _
 		'' report error for 'x \ 0', 'x mod 0'
 		'' Note: no error for 'x / 0', that should just return INF
 		case AST_OP_INTDIV, AST_OP_MOD
-			if( r->con.val.int = 0 ) then
+			if( r->val.i = 0 ) then
 				errReport( FB_ERRMSG_DIVBYZERO )
 			end if
 
 		case AST_OP_POW
-
 			'' convert var ^ 2 to var * var
-			if( r->con.val.float = 2.0 ) then
-
+			if( r->val.f = 2.0 ) then
 				'' operands will be converted to DOUBLE if not floats..
 				if( l->class = AST_NODECLASS_CONV ) then
 					select case l->l->class
@@ -1520,7 +1317,6 @@ function astNewBOP _
 	n->op.options = options
 
 	function = n
-
 end function
 
 '':::::
@@ -1605,12 +1401,7 @@ function astNewSelfBOP _
 
 end function
 
-'':::::
-function astLoadBOP _
-	( _
-		byval n as ASTNODE ptr _
-	) as IRVREG ptr
-
+function astLoadBOP( byval n as ASTNODE ptr ) as IRVREG ptr
     dim as ASTNODE ptr l = any, r = any
     dim as integer op = any
     dim as IRVREG ptr v1 = any, v2 = any, vr = any
@@ -1665,6 +1456,4 @@ function astLoadBOP _
 	astDelNode( r )
 
 	function = vr
-
 end function
-

src/compiler/ast-helper.bas
@@ -925,7 +925,7 @@ private function hConstBound _
 
 	'' dimension is 1-based
 	assert( astGetDataType( dimexpr ) = FB_DATATYPE_INTEGER )
-	dimension = astGetValInt( dimexpr )
+	dimension = astConstGetInt( dimexpr )
 
 	'' Find the referenced dimension
 	if( dimension >= 1 ) then

src/compiler/ast-misc.bas
@@ -127,34 +127,17 @@ function astIsTreeEqual _
 		end if
 
 	case AST_NODECLASS_CONST
-
-		select case as const astGetDataType( l )
-		case FB_DATATYPE_LONGINT, FB_DATATYPE_ULONGINT
-			if( l->con.val.long <> r->con.val.long ) then
+		select case( typeGetClass( l->dtype ) )
+		case FB_DATACLASS_FPOINT
+			if( l->val.f <> r->val.f ) then
 				exit function
 			end if
 
-		case FB_DATATYPE_SINGLE, FB_DATATYPE_DOUBLE
-			if( l->con.val.float <> r->con.val.float ) then
+		case FB_DATACLASS_INTEGER
+			if( l->val.i <> r->val.i ) then
 				exit function
 			end if
 
-  		case FB_DATATYPE_LONG, FB_DATATYPE_ULONG
-  	    	if( FB_LONGSIZE = len( integer ) ) then
-				if( l->con.val.int <> r->con.val.int ) then
-					exit function
-				end if
-  	    	else
-				if( l->con.val.long <> r->con.val.long ) then
-					exit function
-				end if
-  	    	end if
-
-		case else
-			'' bytes/shorts/integers/enums
-			if( l->con.val.int <> r->con.val.int ) then
-				exit function
-			end if
 		end select
 
 	case AST_NODECLASS_DEREF
@@ -389,79 +372,6 @@ function astIsConstant( byval expr as ASTNODE ptr ) as integer
 end function
 
 '':::::
-function astGetValueAsLongInt _
-	( _
-		byval n as ASTNODE ptr _
-	) as longint
-
-	assert( astIsCONST( n ) )
-
-  	select case as const astGetDataType( n )
-  	case FB_DATATYPE_LONGINT, FB_DATATYPE_ULONGINT
-  	    function = astGetValLong( n )
-
-  	case FB_DATATYPE_SINGLE, FB_DATATYPE_DOUBLE
-  		function = clngint( astGetValFloat( n ) )
-
-  	case FB_DATATYPE_LONG, FB_DATATYPE_ULONG
-  	    if( FB_LONGSIZE = len( integer ) ) then
-  			if( astGetDataType( n ) = FB_DATATYPE_LONG ) then
-  				function = clngint( astGetValInt( n ) )
-  			else
-  				function = clngint( cuint( astGetValInt( n ) ) )
-  			end if
-  	   	else
-  	    	function = astGetValLong( n )
-  	    end if
-
-  	case else
-  		if( typeIsSigned( astGetDataType( n ) ) ) then
-  			function = clngint( astGetValInt( n ) )
-  		else
-  			function = clngint( cuint( astGetValInt( n ) ) )
-  		end if
-  	end select
-
-end function
-
-function astGetValueAsDouble( byval n as ASTNODE ptr ) as double
-	assert( astIsCONST( n ) )
-
-	select case as const( astGetDataType( n ) )
-	case FB_DATATYPE_ULONGINT
-		'' without cunsg(), &hFFFFFFFFFFFFFFFFull would be seen as -1,
-		'' causing the double to be -1 instead of the huge value...
-		function = cdbl( cunsg( astGetValLong( n ) ) )
-
-	case FB_DATATYPE_LONGINT
-		function = cdbl( astGetValLong( n ) )
-
-	case FB_DATATYPE_SINGLE, FB_DATATYPE_DOUBLE
-		function = astGetValFloat( n )
-
-	case FB_DATATYPE_ULONG
-		if( FB_LONGSIZE = len( integer ) ) then
-			function = cdbl( cunsg( astGetValLong( n ) ) )
-		else
-			function = cdbl( cunsg( astGetValInt( n ) ) )
-		end if
-
-	case FB_DATATYPE_LONG
-		if( FB_LONGSIZE = len( integer ) ) then
-			function = cdbl( astGetValLong( n ) )
-		else
-			function = cdbl( astGetValInt( n ) )
-		end if
-
-	case FB_DATATYPE_UINT
-		function = cdbl( cunsg( astGetValInt( n ) ) )
-
-	case else
-		function = cdbl( astGetValInt( n ) )
-	end select
-end function
-
-'':::::
 function astGetStrLitSymbol _
 	( _
 		byval n as ASTNODE ptr _
@@ -512,7 +422,7 @@ sub astCheckConst _
 	''    dim b as uinteger = 1 shl 31
 	''
 
-	select case as const( typeGet( dtype ) )
+	select case as const( typeGetDtAndPtrOnly( dtype ) )
 	''case FB_DATATYPE_DOUBLE
 		'' DOUBLE can hold all the other dtype's values;
 		'' perhaps not with 100% precision (e.g. huge ULONGINTs will
@@ -526,7 +436,7 @@ sub astCheckConst _
 		'' min = 1.401298e-45
 		'' max = 3.402823e+38
 
-		dval = astGetValueAsDouble( n )
+		dval = astConstGetAsDouble( n )
 
 		select case abs( dval )
 		case 0.0, 2e-45 to 3e+38 '' definitely no overflow: comfortably within SINGLE bounds
@@ -544,34 +454,33 @@ sub astCheckConst _
 			end if
 		end select
 
-	case FB_DATATYPE_LONGINT, FB_DATATYPE_ULONGINT, _
-	     FB_DATATYPE_INTEGER, FB_DATATYPE_UINT, FB_DATATYPE_ENUM, _
-	     FB_DATATYPE_LONG, FB_DATATYPE_ULONG, _
-	     FB_DATATYPE_SHORT, FB_DATATYPE_USHORT, FB_DATATYPE_WCHAR, _
-	     FB_DATATYPE_BYTE, FB_DATATYPE_UBYTE, FB_DATATYPE_CHAR
+	case FB_DATATYPE_BITFIELD
+		'' !!!WRITEME!!! use ->subtype's
 
-		select case as const( typeGetSize( dtype ) )
-		case 1
-			lval = astGetValueAsLongInt( n )
+	case else
+		select case as const( typeGetSizeType( dtype ) )
+		case FB_SIZETYPE_INT8, FB_SIZETYPE_UINT8
+			lval = astConstGetAsInt64( n )
 			result = ((lval >= -128) and (lval <= 255))
-		case 2
-			lval = astGetValueAsLongInt( n )
+
+		case FB_SIZETYPE_INT16, FB_SIZETYPE_UINT16
+			lval = astConstGetAsInt64( n )
 			result = ((lval >= -32768) and (lval <= 65535))
-		case 4
-			lval = astGetValueAsLongInt( n )
+
+		case FB_SIZETYPE_INT32, FB_SIZETYPE_UINT32
+			lval = astConstGetAsInt64( n )
 			result = ((lval >= -2147483648u) and (lval <= 4294967295u))
-		case 8
+
+		case FB_SIZETYPE_INT64, FB_SIZETYPE_UINT64
 			'' longints can hold most other type's values, except floats
 			'' float?
 			if( typeGetClass( n->dtype ) = FB_DATACLASS_FPOINT ) then
-				dval = astGetValueAsDouble( n )
+				dval = astConstGetAsDouble( n )
 				result = ((dval >= -9223372036854775808ull) and _
 					  (dval <= 18446744073709551615ull))
 			end if
 		end select
 
-	case FB_DATATYPE_BITFIELD
-		'' !!!WRITEME!!! use ->subtype's
 	end select
 
 	if( result = FALSE ) then
@@ -599,7 +508,7 @@ function astPtrCheck _
 		'' Only ok if it's a 0 constant
 		if( astIsCONST( expr ) ) then
 			if( typeGetClass( edtype ) = FB_DATACLASS_INTEGER ) then
-				function = astConstIsZero( expr )
+				function = astConstEqZero( expr )
 			end if
 		end if
 		exit function
@@ -808,7 +717,7 @@ function astBuildBranch _
 		''    over the IF block.
 		'' b) true (or false but inverted), don't emit a jump at all,
 		''    but fall trough to the IF block.
-		if( astConstIsZero( n ) <> is_inverse ) then
+		if( astConstEqZero( n ) <> is_inverse ) then
 			function = astNewBRANCH( AST_OP_JMP, label, NULL )
 		else
 			function = astNewNOP( )

src/compiler/ast-node-check.bas
@@ -32,11 +32,11 @@ function astNewBOUNDCHK _
 			'' index also?
 			if( astIsCONST( l ) ) then
 				'' i < lbound?
-				if( l->con.val.int < lb->con.val.int ) then
+				if( astConstGetInt( l ) < astConstGetInt( lb ) ) then
 					return NULL
 				end if
 				'' i > ubound?
-				if( l->con.val.int > ub->con.val.int ) then
+				if( astConstGetInt( l ) > astConstGetInt( ub ) ) then
 					return NULL
 				end if
 
@@ -47,7 +47,7 @@ function astNewBOUNDCHK _
 		end if
 
 		'' 0? del it
-		if( lb->con.val.int = 0 ) then
+		if( astConstGetInt( lb ) = 0 ) then
 			astDelNode( lb )
 			lb = NULL
 		end if

commit 90fe012ebcf21c56647ad2a4f0024f9719c4bccd

astNewBOP(): Truncate shift amount to 0..bitsize-1
to ensure constant evaluation uses the same shift amount like the
real x86 shift would. (and besides that, this also helps avoiding
undefined behaviour in the C backend)

This fixes a regression from ea511505 since which constant evaluation
is done based on LongInts. This changed the result values produced
by 32bit shr/shl constant evaluation when shifting >= bitsize:
"int64 shl 32" suddenly worked, i.e. returned 0, instead of the
original value as would be done by "int32 shl 32" on x86 (x86 masks
with 31, so 32 ends up being 0, and shift by 0 = original value).

src/compiler/ast-node-bop.bas
@@ -278,8 +278,8 @@ private function hConstBop _
 		case AST_OP_ADD : l->val.i = l->val.i +   r->val.i
 		case AST_OP_SUB : l->val.i = l->val.i -   r->val.i
 		case AST_OP_MUL : l->val.i = l->val.i *   r->val.i
-		case AST_OP_SHL : l->val.i = l->val.i shl r->val.i
-		case AST_OP_SHR : l->val.i = l->val.i shr r->val.i
+		case AST_OP_SHL : l->val.i = l->val.i shl r->val.i : assert( cunsg( r->val.i ) < typeGetBits( l->dtype ) )
+		case AST_OP_SHR : l->val.i = l->val.i shr r->val.i : assert( cunsg( r->val.i ) < typeGetBits( l->dtype ) )
 		case AST_OP_AND : l->val.i = l->val.i and r->val.i
 		case AST_OP_OR  : l->val.i = l->val.i or  r->val.i
 		case AST_OP_XOR : l->val.i = l->val.i xor r->val.i
@@ -317,8 +317,8 @@ private function hConstBop _
 		case AST_OP_ADD : l->val.i = cunsg( l->val.i ) +   cunsg( r->val.i )
 		case AST_OP_SUB : l->val.i = cunsg( l->val.i ) -   cunsg( r->val.i )
 		case AST_OP_MUL : l->val.i = cunsg( l->val.i ) *   cunsg( r->val.i )
-		case AST_OP_SHL : l->val.i = cunsg( l->val.i ) shl cunsg( r->val.i )
-		case AST_OP_SHR : l->val.i = cunsg( l->val.i ) shr cunsg( r->val.i )
+		case AST_OP_SHL : l->val.i = cunsg( l->val.i ) shl cunsg( r->val.i ) : assert( cunsg( r->val.i ) < typeGetBits( l->dtype ) )
+		case AST_OP_SHR : l->val.i = cunsg( l->val.i ) shr cunsg( r->val.i ) : assert( cunsg( r->val.i ) < typeGetBits( l->dtype ) )
 		case AST_OP_AND : l->val.i = cunsg( l->val.i ) and cunsg( r->val.i )
 		case AST_OP_OR  : l->val.i = cunsg( l->val.i ) or  cunsg( r->val.i )
 		case AST_OP_XOR : l->val.i = cunsg( l->val.i ) xor cunsg( r->val.i )
@@ -1196,16 +1196,15 @@ function astNewBOP _
 	'' right-operand must be an integer, so pow2 opts can be done on longint's
 	case AST_OP_SHL, AST_OP_SHR
 		if( astIsCONST( r ) ) then
-			'' warn if shift is greater than or equal to the number of bits in ldtype
-			select case astConstGetAsInt64( r )
-			case 0 to typeGetBits( ldtype )-1
-
-			case else
+			'' Ensure the shift amount is in the range 0..bitsize-1.
+			'' - x86 only supports 0..31 shifts
+			'' - GAS will show errors if outside 0..255 (byte operand)
+			'' - C backend: anything outside the 0..bitsize-1 range is undefined behaviour
+			assert( typeGetClass( r->dtype ) = FB_DATACLASS_INTEGER )
+			if( cunsg( r->val.i ) >= typeGetBits( ldtype ) ) then
 				errReportWarn( FB_WARNINGMSG_SHIFTEXCEEDSBITSINDATATYPE )
-
-				'' prevent gas asm error when value is higher than 255
-				r = astNewBOP(AST_OP_AND, r, astNewCONSTi(255))
-			end select
+				r = astNewBOP( AST_OP_AND, r, astNewCONSTi( typeGetBits( ldtype )-1 ) )
+			end if
 		end if
 
 		if( typeGetDtAndPtrOnly( rdtype ) <> FB_DATATYPE_INTEGER ) then