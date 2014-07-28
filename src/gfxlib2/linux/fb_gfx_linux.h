e3cfee7de95e7f9ba63b271356888926ca587126
dkl <daniel.c.klauer@web.de>
2014-07-29 00:33:07 +0200

    gfxlib2: If #defining DISABLE_FBDEV internally, ensure it's global
    Otherwise gfx_unix.c won't get to see it, and still reference to symbols
    from fb_gfx_linux.h.

--- src/gfxlib2/linux/fb_gfx_linux.h
+++ src/gfxlib2/linux/fb_gfx_linux.h
@@ -3,6 +3,11 @@
 #ifndef __FB_GFX_LINUX_H__
 #define __FB_GFX_LINUX_H__
 
+/* Disable fbdev driver on ARM, because it currently uses x86 inline asm */
+#if !defined HOST_X86 && !defined HOST_X86_64
+	#define DISABLE_FBDEV
+#endif
+
 #ifndef DISABLE_FBDEV
 
 #define DOUBLE_CLICK_TIME		250
