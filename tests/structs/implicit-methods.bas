freebasic/fbc

commit 8b5db5c1f263957c9c507a98176f3efbe672ee38

Determine struct return type after adding implicit members
This fixes an issue where a structure could be seen as trivial despite
non-trivial: hGetReturnType() calls symbCompIsTrivial(), but that depends
on the implicit members being added already or it could produce wrong
results.

The included test case exposes the problem by triggering an assert(),
but only on Win32, because it was trying to return the struct in registers
despite that not being possible for this struct. And it didn't happen on
other systems because those never return structs in registers at all.

src/compiler/symb-proc.bas
@@ -281,6 +281,11 @@ sub symbProcRecalcRealType( byval proc as FBSYMBOL ptr )
 		else
 			dtype = symbGetUDTRetType( subtype )
 
+			'' symbStructEnd() should have set it by now, but there
+			'' could be problems if symbUdtAddDefaultMembers() calls
+			'' this before that.
+			assert( dtype <> FB_DATATYPE_INVALID )
+
 			'' If it became an integer or float, forget the subtype,
 			'' that should only be preserved for UDTs and UDT ptrs.
 			if( typeGetDtOnly( dtype ) <> FB_DATATYPE_STRUCT ) then

src/compiler/symb-struct.bas
@@ -81,9 +81,8 @@ function symbStructBegin _
 	s->udt.natalign = 1
 	s->udt.bitpos = 0
 	s->udt.unpadlgt	= 0
-
+	s->udt.retdtype = FB_DATATYPE_INVALID
 	s->udt.dbg.typenum = INVALID
-
 	s->udt.ext = NULL
 
 	'' extending another UDT?
@@ -774,11 +773,13 @@ sub symbStructEnd _
 		sym->lgt += pad
 	end if
 
-	'' set the real data type used to return this struct from procs
-	sym->udt.retdtype = hGetReturnType( sym )
-
 	'' Declare & add any implicit members
 	symbUdtAddDefaultMembers( sym )
+
+	'' Determine how to return this structure from procedures
+	'' (must be done after adding default members because it depends on
+	'' symbCompIsTrivial() which depends on knowing all ctors/dtors)
+	sym->udt.retdtype = hGetReturnType( sym )
 
 	'' check for forward references
 	if( symb.fwdrefcnt > 0 ) then

tests/functions/return-non-trivial.bas
@@ -1,0 +1,42 @@
+# include "fbcu.bi"
+
+namespace fbc_tests.structs.return_non_trivial
+
+namespace nonTrivialDueToField
+	'' UDTs can become non-trivial as a result of containing a non-trivial
+	'' field. The compiler must be careful to take this into account when
+	'' determining how to return an UDT from a function (in registers or
+	'' on stack).
+
+	dim shared dtors as integer
+
+	type A
+		i as integer
+		declare destructor( )
+	end type
+
+	destructor A()
+		dtors += 1
+	end destructor
+
+	type B
+		a as A
+	end type
+
+	function f( ) as B
+		function = type( (123) )
+	end function
+
+	sub test cdecl( )
+		CU_ASSERT( dtors = 0 )
+		CU_ASSERT( f().a.i = 123 )
+		CU_ASSERT( dtors = 2 )
+	end sub
+end namespace
+
+private sub ctor( ) constructor
+	fbcu.add_suite( "tests/structs/return-non-trivial" )
+	fbcu.add_test( "nonTrivialDueToField", @nonTrivialDueToField.test )
+end sub
+
+end namespace

commit 5940145a9a16fb732cd5dbc8c78ef629ecf59d6b

Implement implicit UDT members after calculating UDT return type
This fixes an issue where the implementation of implicit UDT members
(specifically, building the vtable) wanted to use the UDT return type
(due to a virtual method with byval self function result), but could not
because it was not known yet (it triggered the assert() in
symbProcRecalcRealType() when creating the function pointer for the
vtable slot).

This is a regression from 8b5db5c1 where the order of
adding the implicits and calculating the UDT return type was changed.

The solution to fix both cases (the original issue from the old commit,
and the regression introduced by the old fix) is to split declaration from
implementation of implicit members, and calculate the UDT return type in
between.

src/compiler/parser-decl-struct.bas
@@ -898,7 +898,7 @@ private sub hCheckForCDtorOrMethods( byval sym as FBSYMBOL ptr )
 		'' implemented since we don't allow nested procedures.
 		'' (Note: assuming symbStructEnd() was already called,
 		'' thus any implicit members were already added by
-		'' symbUdtAddDefaultMembers())
+		'' symbUdtDeclareDefaultMembers())
 		member = symbGetCompSymbTb( sym ).head
 		while( member )
 			if( symbIsProc( member ) ) then

src/compiler/symb-comp.bas
@@ -509,10 +509,13 @@ private sub hAddLetOpBody _
 
 end sub
 
-'' Declare & add any implicit/default members and global vars if needed
-sub symbUdtAddDefaultMembers( byval udt as FBSYMBOL ptr )
-	dim as FBSYMBOL ptr defctor = any, dtor = any, copyctor = any, _
-		copyctorconst = any, copyletopconst = any
+'' Declare any implicit/default UDT members without implementing them.
+sub symbUdtDeclareDefaultMembers _
+	( _
+		byref default as SYMBDEFAULTMEMBERS, _
+		byval udt as FBSYMBOL ptr _
+	)
+
 	dim as integer missing_base_defctor = any
 
 	''
@@ -556,11 +559,11 @@ sub symbUdtAddDefaultMembers( byval udt as FBSYMBOL ptr )
 		missing_base_defctor = FALSE
 	end if
 
-	defctor = NULL
-	copyctor = NULL
-	copyctorconst = NULL
-	copyletopconst = NULL
-	dtor = NULL
+	default.defctor = NULL
+	default.copyctor = NULL
+	default.copyctorconst = NULL
+	default.copyletopconst = NULL
+	default.dtor = NULL
 
 	'' Ctor/inited fields and no ctor yet?
 	if( (symbGetUDTHasCtorField( udt ) or symbGetUDTHasInitedField( udt )) and _
@@ -573,7 +576,7 @@ sub symbUdtAddDefaultMembers( byval udt as FBSYMBOL ptr )
 			errReport( FB_ERRMSG_NEEDEXPLICITDEFCTOR )
 		else
 			'' Add default ctor
-			defctor = hDeclareProc( udt, INVALID, FB_DATATYPE_INVALID, FB_SYMBATTRIB_OVERLOADED or FB_SYMBATTRIB_CONSTRUCTOR )
+			default.defctor = hDeclareProc( udt, INVALID, FB_DATATYPE_INVALID, FB_SYMBATTRIB_OVERLOADED or FB_SYMBATTRIB_CONSTRUCTOR )
 		end if
 	end if
 
@@ -585,8 +588,8 @@ sub symbUdtAddDefaultMembers( byval udt as FBSYMBOL ptr )
 
 		if( udt->udt.ext->copyletopconst = NULL ) then
 			'' declare operator let( byref rhs as const UDT )
-			copyletopconst = hDeclareProc( udt, AST_OP_ASSIGN, typeSetIsConst( FB_DATATYPE_STRUCT ), FB_SYMBATTRIB_OVERLOADED or FB_SYMBATTRIB_OPERATOR )
-			symbProcCheckOverridden( copyletopconst, TRUE )
+			default.copyletopconst = hDeclareProc( udt, AST_OP_ASSIGN, typeSetIsConst( FB_DATATYPE_STRUCT ), FB_SYMBATTRIB_OVERLOADED or FB_SYMBATTRIB_OPERATOR )
+			symbProcCheckOverridden( default.copyletopconst, TRUE )
 		end if
 
 		if( udt->udt.ext->copyctorconst = NULL ) then
@@ -596,7 +599,7 @@ sub symbUdtAddDefaultMembers( byval udt as FBSYMBOL ptr )
 				'' same as with default ctor above.
 				errReport( FB_ERRMSG_NEEDEXPLICITCOPYCTORCONST )
 			else
-				copyctorconst = hDeclareProc( udt, INVALID, typeSetIsConst( FB_DATATYPE_STRUCT ), FB_SYMBATTRIB_OVERLOADED or FB_SYMBATTRIB_CONSTRUCTOR )
+				default.copyctorconst = hDeclareProc( udt, INVALID, typeSetIsConst( FB_DATATYPE_STRUCT ), FB_SYMBATTRIB_OVERLOADED or FB_SYMBATTRIB_CONSTRUCTOR )
 			end if
 		end if
 
@@ -609,7 +612,7 @@ sub symbUdtAddDefaultMembers( byval udt as FBSYMBOL ptr )
 				'' same as with default ctor above.
 				errReport( FB_ERRMSG_NEEDEXPLICITCOPYCTOR )
 			else
-				copyctor = hDeclareProc( udt, INVALID, FB_DATATYPE_STRUCT, FB_SYMBATTRIB_OVERLOADED or FB_SYMBATTRIB_CONSTRUCTOR )
+				default.copyctor = hDeclareProc( udt, INVALID, FB_DATATYPE_STRUCT, FB_SYMBATTRIB_OVERLOADED or FB_SYMBATTRIB_CONSTRUCTOR )
 			end if
 		end if
 	end if
@@ -621,12 +624,22 @@ sub symbUdtAddDefaultMembers( byval udt as FBSYMBOL ptr )
 		'' no default dtor explicitly defined?
 		if( udt->udt.ext->dtor = NULL ) then
 			'' Dtor
-			dtor = hDeclareProc( udt, INVALID, FB_DATATYPE_INVALID, FB_SYMBATTRIB_DESTRUCTOR )
+			default.dtor = hDeclareProc( udt, INVALID, FB_DATATYPE_INVALID, FB_SYMBATTRIB_DESTRUCTOR )
 
 			'' Don't allow the implicit dtor to override a FINAL dtor from the base
-			symbProcCheckOverridden( dtor, TRUE )
+			symbProcCheckOverridden( default.dtor, TRUE )
 		end if
 	end if
+
+end sub
+
+'' Implement the implicit members declared by symbUdtDeclareDefaultMembers(),
+'' add other implicit code (vtable/rtti global vars).
+sub symbUdtImplementDefaultMembers _
+	( _
+		byref default as SYMBDEFAULTMEMBERS, _
+		byval udt as FBSYMBOL ptr _
+	)
 
 	''
 	'' Add vtable and rtti global variables
@@ -659,25 +672,27 @@ sub symbUdtAddDefaultMembers( byval udt as FBSYMBOL ptr )
 	''
 	'' Add bodies if any implicit ctor/dtor/let procs were declared above
 	''
-	if( defctor ) then
-		hAddCtorBody( udt, defctor, FALSE )
+
+	if( default.defctor ) then
+		hAddCtorBody( udt, default.defctor, FALSE )
 	end if
 
-	if( copyletopconst ) then
-		hAddLetOpBody( udt, copyletopconst )
+	if( default.copyletopconst ) then
+		hAddLetOpBody( udt, default.copyletopconst )
 	end if
 
-	if( copyctorconst ) then
-		hAddCtorBody( udt, copyctorconst, TRUE )
+	if( default.copyctorconst ) then
+		hAddCtorBody( udt, default.copyctorconst, TRUE )
 	end if
 
-	if( copyctor ) then
-		hAddCtorBody( udt, copyctor, TRUE )
+	if( default.copyctor ) then
+		hAddCtorBody( udt, default.copyctor, TRUE )
 	end if
 
-	if( dtor ) then
-		hAddCtorBody( udt, dtor, FALSE )
+	if( default.dtor ) then
+		hAddCtorBody( udt, default.dtor, FALSE )
 	end if
+
 end sub
 
 ''::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::

src/compiler/symb-proc.bas
@@ -282,8 +282,8 @@ sub symbProcRecalcRealType( byval proc as FBSYMBOL ptr )
 			dtype = symbGetUDTRetType( subtype )
 
 			'' symbStructEnd() should have set it by now, but there
-			'' could be problems if symbUdtAddDefaultMembers() calls
-			'' this before that.
+			'' could be problems if symbUdtDeclareDefaultMembers()
+			'' calls this before that.
 			assert( dtype <> FB_DATATYPE_INVALID )
 
 			'' If it became an integer or float, forget the subtype,
@@ -2742,7 +2742,7 @@ sub symbProcCheckOverridden _
 		if( errmsg <> FB_ERRMSG_OK ) then
 			if( is_implicit and _
 			    (errmsg = FB_ERRMSG_OVERRIDECALLCONVDIFFERS) ) then
-				'' symbUdtAddDefaultMembers() uses this to check
+				'' symbUdtDeclareDefaultMembers() uses this to check
 				'' implicit dtors and LET overloads. Since they
 				'' are not visible in the original code,
 				'' the error message must have more info.

src/compiler/symb-struct.bas
@@ -759,6 +759,7 @@ sub symbStructEnd _
 		byval isnested as integer _
 	)
 
+	dim as SYMBDEFAULTMEMBERS defaultmembers = any
 	dim as integer pad = any
 
 	'' end nesting?
@@ -776,13 +777,28 @@ sub symbStructEnd _
 		sym->lgt += pad
 	end if
 
-	'' Declare & add any implicit members
-	symbUdtAddDefaultMembers( sym )
+	'' Declare implicit members (but don't implement them yet)
+	symbUdtDeclareDefaultMembers( defaultmembers, sym )
 
 	'' Determine how to return this structure from procedures
-	'' (must be done after adding default members because it depends on
+	'' (must be done after declaring default members because it depends on
 	'' symbCompIsTrivial() which depends on knowing all ctors/dtors)
 	sym->udt.retdtype = hGetReturnType( sym )
+
+	''
+	'' Now that the UDT return type is known we can build code using it,
+	'' such as the implicit methods' bodies, or the function pointers for
+	'' the vtable slots.
+	''
+	'' Methods declared by the user using the UDT itself will be fixed up
+	'' by hPatchByvalParamsToSelf/hPatchByvalResultToSelf, but that only
+	'' works for declarations, not implementations.
+	''
+	'' Furthermore, function pointers (such as the vtable ones) can be added
+	'' to a different namespace, i.e. not the UDT, so the hPatch* functions
+	'' wouldn't handle them anyways.
+	''
+	symbUdtImplementDefaultMembers( defaultmembers, sym )
 
 	'' check for forward references
 	if( symb.fwdrefcnt > 0 ) then

src/compiler/symb.bi
@@ -815,6 +815,14 @@ type SYMB_DATATYPE
 	name			as const zstring ptr
 end type
 
+'' Data passed from symbUdtDeclareDefaultMembers() to symbUdtImplementDefaultMembers()
+type SYMBDEFAULTMEMBERS
+	defctor		as FBSYMBOL ptr
+	dtor		as FBSYMBOL ptr
+	copyctor	as FBSYMBOL ptr
+	copyctorconst	as FBSYMBOL ptr
+	copyletopconst	as FBSYMBOL ptr
+end type
 
 #include once "ast.bi"
 
@@ -1729,7 +1737,19 @@ declare sub symbSetDefType _
 	)
 
 declare sub symbUdtAllocExt( byval udt as FBSYMBOL ptr )
-declare sub symbUdtAddDefaultMembers( byval sym as FBSYMBOL ptr )
+
+declare sub symbUdtDeclareDefaultMembers _
+	( _
+		byref default as SYMBDEFAULTMEMBERS, _
+		byval udt as FBSYMBOL ptr _
+	)
+
+declare sub symbUdtImplementDefaultMembers _
+	( _
+		byref default as SYMBDEFAULTMEMBERS, _
+		byval udt as FBSYMBOL ptr _
+	)
+
 declare function symbCompIsTrivial( byval sym as FBSYMBOL ptr ) as integer
 declare sub symbSetCompCtorHead( byval sym as FBSYMBOL ptr, byval proc as FBSYMBOL ptr )
 declare sub symbCheckCompCtor( byval sym as FBSYMBOL ptr, byval proc as FBSYMBOL ptr )

tests/structs/implicit-methods.bas
@@ -508,6 +508,48 @@ namespace copyLetConstFwdref2
 	end sub
 end namespace
 
+namespace virtualWithByvalSelfResult
+	'' An UDT with a virtual method that returns the UDT itself byval.
+	''
+	'' fbc must be careful to declare implicit members, calculate the UDT
+	'' return type, and implement implicit members in the right order,
+	'' otherwise some code (such as the function pointers for vtable slots)
+	'' may try to use the UDT return type while it's not known yet.
+
+	type UDT extends object
+		i as integer = 123
+		declare virtual function f() as UDT
+	end type
+
+	function UDT.f() as UDT
+		function = type()
+	end function
+
+	sub test cdecl( )
+		dim as UDT a, b
+		CU_ASSERT( a.i = 123 )
+		CU_ASSERT( b.i = 123 )
+
+		a.i = 0
+		b.i = 0
+		CU_ASSERT( a.i = 0 )
+		CU_ASSERT( b.i = 0 )
+
+		b = a.f()
+		CU_ASSERT( a.i = 0 )
+		CU_ASSERT( b.i = 123 )
+
+		a.i = 0
+		b.i = 0
+		CU_ASSERT( a.i = 0 )
+		CU_ASSERT( b.i = 0 )
+
+		a = a.f()
+		CU_ASSERT( a.i = 123 )
+		CU_ASSERT( b.i = 0 )
+	end sub
+end namespace
+
 private sub ctor( ) constructor
 	fbcu.add_suite( "tests/structs/implicit-methods" )
 
@@ -530,6 +572,8 @@ private sub ctor( ) constructor
 	fbcu.add_test( "copyLetFwdref2"     , @copyLetFwdref2.test )
 	fbcu.add_test( "copyLetConstFwdref1", @copyLetConstFwdref1.test )
 	fbcu.add_test( "copyLetConstFwdref2", @copyLetConstFwdref2.test )
+
+	fbcu.add_test( "virtualWithByvalSelfResult", @virtualWithByvalSelfResult.test )
 end sub
 
 end namespace