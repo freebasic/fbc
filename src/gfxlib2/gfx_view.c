54d999bb6fc7ea4edc9e7f56fa97f601d2021732
dkl <daniel.c.klauer@web.de>
2015-01-31 16:22:32 +0100

    gfxlib2: Clip View [Screen] coordinates to screen (fixes #768)
    Previously, certain coordinates specified for a View [Screen] could cause
    the context->view_w|h to be bigger than __fb_gfx->w|h, which causes
    subsequent drawing commands to do wrong clipping themselves, resulting in
    potential buffer overruns.
    
    Example of how the old code could go wrong:
    	screenres 320, 240
    	view screen (-20,-20)-(340,260)
    
    	x2 - x1 + 1 = 361
    	__fb_gfx->w - x1 = 340
    
    	After MIN(), context->view_w = 340,
    	which is > the available screen,
    	causing buffer overruns in subsequent drawing commands.

--- src/gfxlib2/gfx_view.c
+++ src/gfxlib2/gfx_view.c
@@ -21,6 +21,15 @@ FBCALL void fb_GfxView(int x1, int y1, int x2, int y2, unsigned int fill_color, 
 	fb_hFixCoordsOrder(&x1, &y1, &x2, &y2);
 
     if ((x1 | y1 | x2 | y2) != (int)0xFFFF8000) {
+        /* Do nothing if viewport is completely off-screen, otherwise the
+           clipping done below would move the rectangle instead of just clipping
+           it. It may be better to trigger an "Illegal function call" here, but
+           there's no error return here currently. */
+        if (x1 >= __fb_gfx->w || y1 >= __fb_gfx->h || x2 < 0 || y2 < 0) {
+            FB_GRAPHICS_UNLOCK( );
+            return;
+        }
+
         context->flags |= CTX_VIEWPORT_SET;
 
         if (flags & VIEW_SCREEN)
@@ -38,10 +47,28 @@ FBCALL void fb_GfxView(int x1, int y1, int x2, int y2, unsigned int fill_color, 
             fb_hGfxBox(x1 - 1, y1 - 1, x2 + 1, y2 + 1, border_color & __fb_gfx->color_mask, FALSE, 0xFFFF);
         }
 
-        context->view_x = MID(0, x1, __fb_gfx->w);
-        context->view_y = MID(0, y1, __fb_gfx->h);
-        context->view_w = MIN(x2 - x1 + 1, __fb_gfx->w - x1);
-        context->view_h = MIN(y2 - y1 + 1, __fb_gfx->h - y1);
+        /* Clip viewport to screen (without "moving" the viewport). The drawing
+           commands rely on this to avoid buffer overruns.
+
+           A "view" with off-screen parts doesn't make sense since it's supposed
+           to restrict drawing to some area of the screen, but we have to deal
+           with such "bad input" coordinates nevertheless, if they were given by
+           the program. Clipping seems like a good solution. */
+
+        x1 = MID(0, x1, __fb_gfx->w - 1);
+        y1 = MID(0, y1, __fb_gfx->h - 1);
+        x2 = MID(0, x2, __fb_gfx->w - 1);
+        y2 = MID(0, y2, __fb_gfx->h - 1);
+
+        context->view_x = x1;
+        context->view_y = y1;
+        context->view_w = x2 - x1 + 1;
+        context->view_h = y2 - y1 + 1;
+
+        DBG_ASSERT(context->view_x >= 0 && context->view_x < __fb_gfx->w);
+        DBG_ASSERT(context->view_y >= 0 && context->view_y < __fb_gfx->h);
+        DBG_ASSERT(context->view_w > 0 && context->view_w <= __fb_gfx->w);
+        DBG_ASSERT(context->view_h > 0 && context->view_h <= __fb_gfx->h);
 
         if (!(flags & DEFAULT_COLOR_1)) {
             old_bg_color = context->bg_color;
