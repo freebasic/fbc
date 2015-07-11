freebasic/fbc

commit c4a4dcfcde42269d97c1b555c10b9e5e70c87b2d

boolean: allow redefinition (with a warning) of intrinsic constants

src/compiler/symb-const.bas
@@ -50,49 +50,84 @@ function symbReuseOrAddConst _
 		byval attrib as integer _
 	) as FBSYMBOL ptr
 
+	dim is_same as integer = FALSE
+
+	function = NULL
+
 	var sym = symbAddConst( id, dtype, subtype, value, attrib )
+	
 	if( sym = NULL ) then
-		'' Duplicate definition; allow it if the existing symbol is also
-		'' a CONST and has the same dtype and value.
+
+		'' Duplicate definition; allow it if the existing symbol is 
+		'' also a CONST and has the same dtype and value.  If it does
+		'' not have same dtype and value, allow it anyway if it is a 
+		'' symbol that may be redefined.
 		sym = symbLookupByNameAndClass( symbGetCurrentNamespc( ), id, FB_SYMBCLASS_CONST, FALSE, FALSE )
 		assert( sym )
 
-		if( (sym->typ <> dtype) or (sym->subtype <> subtype) ) then
-			exit function
+		'' same type?
+		if( (sym->typ = dtype) and (sym->subtype = subtype) ) then
+
+			'' same value?
+			select case( typeGetDtAndPtrOnly( dtype ) )
+			case FB_DATATYPE_STRING, FB_DATATYPE_CHAR, FB_DATATYPE_WCHAR
+				if( value->s = sym->val.s ) then
+					is_same = TRUE
+				end if
+
+				#if __FB_DEBUG__
+					assert( value->s->class = FB_SYMBCLASS_VAR )
+					assert( sym->val.s->class = FB_SYMBCLASS_VAR )
+					if( typeGetDtAndPtrOnly( dtype ) = FB_DATATYPE_WCHAR ) then
+						assert( *value->s->var_.littextw = *sym->val.s->var_.littextw )
+					else
+						assert( *value->s->var_.littext = *sym->val.s->var_.littext )
+					end if
+				#endif
+
+			case FB_DATATYPE_SINGLE, FB_DATATYPE_DOUBLE
+				'' Redundant float CONSTs are disallowed for now,
+				'' because comparing floats is unreliable
+				exit function
+
+				'' TODO: compare with epsilon? probably still unreliable
+				'' instead, implement byte-by-byte comparison (big/little endian won't matter)
+				'if( memcmp( value->f, sym->val.f, sizeof(FBVALUE.f) ) = 0 ) then
+				'	is_same = TRUE
+				'end if
+				
+			case else
+				assert( typeGetClass( dtype ) = FB_DATACLASS_INTEGER )
+				if( value->i = sym->val.i ) then
+					is_same = TRUE
+				end if
+			end select
 		end if
 
-		select case( typeGetDtAndPtrOnly( dtype ) )
-		case FB_DATATYPE_STRING, FB_DATATYPE_CHAR, FB_DATATYPE_WCHAR
-			if( value->s <> sym->val.s ) then
-				exit function
-			end if
-
-			#if __FB_DEBUG__
-				assert( value->s->class = FB_SYMBCLASS_VAR )
-				assert( sym->val.s->class = FB_SYMBCLASS_VAR )
-				if( typeGetDtAndPtrOnly( dtype ) = FB_DATATYPE_WCHAR ) then
-					assert( *value->s->var_.littextw = *sym->val.s->var_.littextw )
-				else
-					assert( *value->s->var_.littext = *sym->val.s->var_.littext )
+		'' even if there is a name-type-value collision, may still be allowed
+		if( is_same = FALSE ) then
+			'' redefinition allowed?
+			if( symbGetCanRedef( sym ) ) then
+				'' only global namespace symbols can be redefined
+				if( symbGetNamespace( sym ) = @symbGetGlobalNamespc( ) ) then
+					if( symbGetCurrentNamespc( ) = @symbGetGlobalNamespc( ) ) then
+						if( symbGetCantUndef( sym ) = FALSE ) then
+							errReportWarn( FB_WARNINGMSG_REDEFINITIONOFINTRINSIC )
+							'' remove from lookup, but not type symbol data, it
+							'' may be referenced from elsewhere
+							symbDelFromHash( sym )
+							'' try again
+							sym = symbAddConst( id, dtype, subtype, value, attrib )
+						end if
+					end if
 				end if
-			#endif
-
-		case FB_DATATYPE_SINGLE, FB_DATATYPE_DOUBLE
-			'' Redundant float CONSTs are disallowed for now,
-			'' because comparing floats is unreliable
-			exit function
-
-			'' TODO: compare with epsilon? probably still unreliable
-			'if( value->f <> sym->val.f ) then
-			'	exit function
-			'end if
-
-		case else
-			assert( typeGetClass( dtype ) = FB_DATACLASS_INTEGER )
-			if( value->i <> sym->val.i ) then
-				exit function
+			else
+				'' duplicate definition
+				sym = NULL
 			end if
-		end select
+		else
+			sym->stats and= not FB_SYMBSTATS_CANREDEFINE
+		end if
 	end if
 
 	function = sym

src/compiler/error.bas
@@ -79,7 +79,8 @@ declare function hMakeParamDesc _
 		( 0, @"'=' parsed as equality operator in function argument, not assignment to BYREF function result" ), _
 		( 0, @"Mixing signed/unsigned operands" ), _
 		( 0, @"Mismatching parameter initializer" ), _
-		( 0, @"Mixing operand data types may have undefined results" ) _
+		( 0, @"Mixing operand data types may have undefined results" ), _
+		( 0, @"Redefinition of intrinsic" ) _
 	}
 
 	dim shared errorMsgs( 1 to FB_ERRMSGS-1 ) as const zstring ptr => _

src/compiler/error.bi
@@ -364,6 +364,7 @@ enum FB_WARNINGMSG
 	FB_WARNINGMSG_OPERANDSMIXEDSIGNEDNESS
 	FB_WARNINGMSG_MISMATCHINGPARAMINIT
 	FB_WARNINGMSG_OPERANDSMIXEDTYPES
+	FB_WARNINGMSG_REDEFINITIONOFINTRINSIC
 
 	FB_WARNINGMSGS
 end enum

src/compiler/pp-define.bas
@@ -1043,9 +1043,22 @@ sub ppDefine( byval ismultiline as integer )
 		sym = chain_->sym
 		if( symbIsDefine( sym ) = FALSE ) then
 			'' defines have no dups or respect namespaces
-			errReportEx( FB_ERRMSG_DUPDEFINITION, @defname )
-			'' error recovery: fake an id
-			defname = *symbUniqueLabel( )
+			if( symbGetCanRedef( sym ) ) then
+				'' only global namespace symbols can be redefined
+				if( symbGetNamespace( sym ) = @symbGetGlobalNamespc( ) ) then
+					if( symbGetCantUndef( sym ) = FALSE ) then
+						errReportWarn( FB_WARNINGMSG_REDEFINITIONOFINTRINSIC )
+						'' remove from lookup, but not type symbol data, it
+						'' may be referenced from elsewhere
+						symbDelFromHash( sym )
+						sym = NULL
+					end if
+				end if
+			else
+				errReportEx( FB_ERRMSG_DUPDEFINITION, @defname )
+				'' error recovery: fake an id
+				defname = *symbUniqueLabel( )
+			end if
 		end if
 	else
 		sym = NULL

src/compiler/symb-keyword.bas
@@ -367,8 +367,9 @@ sub symbKeywordConstsInit( )
 	dim id as string * 10
 
 	dim as FB_SYMBATTRIB attrib = any
+	dim as FBSYMBOL ptr sym = any
 
-	attrib = FB_SYMBATTRIB_SHARED or FB_SYMBATTRIB_CONST or FB_SYMBATTRIB_LITERAL
+	attrib = FB_SYMBATTRIB_CONST or FB_SYMBATTRIB_LITERAL
 
 	v.i = cint(0)
 	if( fbLangIsSet( FB_LANG_QB ) ) then
@@ -376,7 +377,11 @@ sub symbKeywordConstsInit( )
 	else
 		id = kwdFALSE
 	end if
-	symbAddConst( strptr(id), FB_DATATYPE_BOOLEAN, NULL, @v, attrib )
+	sym = symbAddConst( strptr(id), FB_DATATYPE_BOOLEAN, NULL, @v, attrib )
+	'' Success? ... symbAddConst() may fail if id was defined on the command line
+	if( sym ) then
+		symbSetCanRedef( sym )
+	end if
 
 	v.i = cint(-1)
 	if( fbLangIsSet( FB_LANG_QB ) ) then
@@ -384,6 +389,10 @@ sub symbKeywordConstsInit( )
 	else
 		id = kwdTRUE
 	end if
-	symbAddConst( strptr(id), FB_DATATYPE_BOOLEAN, NULL, @v, attrib )
+	'' Success? ... symbAddConst() may fail if id was defined on the command line
+	sym = symbAddConst( strptr(id), FB_DATATYPE_BOOLEAN, NULL, @v, attrib )
+	if( sym ) then
+		symbSetCanRedef( sym )
+	end if
 
 end sub

src/compiler/symb.bi
@@ -124,7 +124,7 @@ enum FB_SYMBSTATS
     FB_SYMBSTATS_GLOBALDTOR   = &h00020000
     FB_SYMBSTATS_CANTDUP      = &h00040000
     FB_SYMBSTATS_CANBECLONED  = &h00080000  '' procedures: It's safe to duplicate CALLs to procedures with this flag, no problems with side effects. Useful for RTL procedures where we can be sure about this.
-                            ''= &h00100000
+    FB_SYMBSTATS_CANREDEFINE  = &h00100000  '' CONSTs: implicitly generated by compiler (symbKeywordConstsInit()), may be redefined
                             ''= &h00200000
     FB_SYMBSTATS_HASRTTI      = &h00400000
     FB_SYMBSTATS_CANTUNDEF    = &h00800000
@@ -1985,6 +1985,9 @@ declare function symbGetUDTBaseLevel _
 
 #define symbGetCantUndef(s) ((s->stats and FB_SYMBSTATS_CANTUNDEF) <> 0)
 #define symbSetCantUndef(s) s->stats or= FB_SYMBSTATS_CANTUNDEF
+
+#define symbGetCanRedef(s) ((s->stats and FB_SYMBSTATS_CANREDEFINE) <> 0)
+#define symbSetCanRedef(s) s->stats or= FB_SYMBSTATS_CANREDEFINE
 
 #define symbGetIsUnionField(s) ((s->stats and FB_SYMBSTATS_UNIONFIELD) <> 0)
 #define symbSetIsUnionField(s) s->stats or= FB_SYMBSTATS_UNIONFIELD

commit 292da0da1944fc8d176fd8e23ad0e458151cd687

internal: symbReuseOrAddConst(): Remove string debugging code
It was partially broken by c4a4dcfc, because now it will even be reached
in case the constant values are different, causing the assert()'s to fire
during the log-tests.

But these asserts() don't seem useful anymore anyways (no point checking
that the symbol has the same string data if we know it's the same symbol
anyways), so it's probably best to just remove them completely.

src/compiler/symb-const.bas
@@ -71,19 +71,11 @@ function symbReuseOrAddConst _
 			'' same value?
 			select case( typeGetDtAndPtrOnly( dtype ) )
 			case FB_DATATYPE_STRING, FB_DATATYPE_CHAR, FB_DATATYPE_WCHAR
+				'' Compare the string literal symbol (global VAR),
+				'' symbAllocStrConst() will have re-used the same symbol if it's the same string.
 				if( value->s = sym->val.s ) then
 					is_same = TRUE
 				end if
-
-				#if __FB_DEBUG__
-					assert( value->s->class = FB_SYMBCLASS_VAR )
-					assert( sym->val.s->class = FB_SYMBCLASS_VAR )
-					if( typeGetDtAndPtrOnly( dtype ) = FB_DATATYPE_WCHAR ) then
-						assert( *value->s->var_.littextw = *sym->val.s->var_.littextw )
-					else
-						assert( *value->s->var_.littext = *sym->val.s->var_.littext )
-					end if
-				#endif
 
 			case FB_DATATYPE_SINGLE, FB_DATATYPE_DOUBLE
 				'' Redundant float CONSTs are disallowed for now,