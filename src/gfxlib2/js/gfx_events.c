f75bfb6bdfd43e65f892ba8f310d413086b2993b
v1ctor <av1ctor@yahoo.com.br>
2015-10-12 00:09:24 -0300

    fixed: some extended keys

--- src/gfxlib2/js/gfx_events.c
+++ src/gfxlib2/js/gfx_events.c
@@ -1,6 +1,8 @@
 #include "../fb_gfx.h"
 #include "fb_gfx_js.h"
 
+static int sleep_called = 0;
+
 static int fb_js_gfx_Getkey( void )
 {
 	if( __fb_js_ctx.key_head == __fb_js_ctx.key_tail)
@@ -31,7 +33,11 @@ static FBSTRING *fb_js_gfx_Inkey( void )
 
 static void fb_js_key_down(SDL_Event *event)
 {
-    __fb_js_ctx.key_buffer[__fb_js_ctx.key_tail] = event->key.keysym.sym;
+    unsigned long key = event->key.keysym.sym;
+
+    //emscripten_log(EM_LOG_CONSOLE, "%d, %d, %d", event->key.keysym.sym, event->key.keysym.scancode, key > 0x00FF? ((key & 0x00FF) << 8 | 0x00FF): key);
+
+    __fb_js_ctx.key_buffer[__fb_js_ctx.key_tail] = key > 0x00FF? ((key & 0x00FF) << 8 | 0x00FF): key;
     __fb_js_ctx.key_tail = (__fb_js_ctx.key_tail + 1) % KEY_BUFFER_LEN;
 
     if( __fb_js_ctx.key_tail == __fb_js_ctx.key_head )
@@ -65,13 +71,22 @@ void fb_js_events_check(void)
     }
 }
 
+static void fb_js_sleep(int msecs)
+{
+    if( !sleep_called )
+    {
+        sleep_called = 1;
+        emscripten_log(EM_LOG_WARN, "Warning: Call to SLEEP() ignored. It should not be used in Javascript");
+    }
+}
+
 void fb_js_events_init(void)
 {
 	__fb_ctx.hooks.inkeyproc  = fb_js_gfx_Inkey;
 	__fb_ctx.hooks.getkeyproc = fb_js_gfx_Getkey;
 	__fb_ctx.hooks.keyhitproc = fb_js_gfx_KeyHit;
 	__fb_ctx.hooks.multikeyproc = NULL;
-	__fb_ctx.hooks.sleepproc = NULL;
+	__fb_ctx.hooks.sleepproc = fb_js_sleep;
 
 	return;
 }
