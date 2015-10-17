3f1e173cea2ce44c2912b38429c474184e63d31c
v1ctor <av1ctor@yahoo.com.br>
2015-10-16 23:29:19 -0300

    changed: terminal is now emulated using termlib.js

--- src/rtlib/js/hinit.c
+++ src/rtlib/js/hinit.c
@@ -2,8 +2,6 @@
 
 #include "../fb.h"
 #include "fb_private_console.h"
-#include <emscripten/emscripten.h>
-#include <emscripten/html5.h>
 
 FB_CONSOLE_CTX __fb_con;
 
@@ -18,22 +16,22 @@ FBCALL void fb_GraphicsUnlock( void ) { return; }
 
 static const char color_remap[16] =
 {
-    [FB_COLOR_BLACK] = '1',
-    [FB_COLOR_BLUE] = 'D',
-    [FB_COLOR_GREEN] = 'B',
-    [FB_COLOR_CYAN] = 'F',
-    [FB_COLOR_RED] = 'A',
-    [FB_COLOR_MAGENTA] = 'E',
-    [FB_COLOR_BROWN] = 'C',
-    [FB_COLOR_WHITE] = '8',
-    [FB_COLOR_GREY] = '9',
-    [FB_COLOR_LBLUE] = '5',
-    [FB_COLOR_LGREEN] = '3',
-    [FB_COLOR_LCYAN] = '7',
-    [FB_COLOR_LRED] = '2',
-    [FB_COLOR_LMAGENTA] = '6',
-    [FB_COLOR_YELLOW] = '4',
-    [FB_COLOR_BWHITE] = '8',
+    [FB_COLOR_BLACK] = 0x1,
+    [FB_COLOR_BLUE] = 0xD,
+    [FB_COLOR_GREEN] = 0xB,
+    [FB_COLOR_CYAN] = 0xF,
+    [FB_COLOR_RED] = 0xA,
+    [FB_COLOR_MAGENTA] = 0xE,
+    [FB_COLOR_BROWN] = 0xC,
+    [FB_COLOR_WHITE] = 0x8,
+    [FB_COLOR_GREY] = 0x9,
+    [FB_COLOR_LBLUE] = 0x5,
+    [FB_COLOR_LGREEN] = 0x3,
+    [FB_COLOR_LCYAN] = 0x7,
+    [FB_COLOR_LRED] = 0x2,
+    [FB_COLOR_LMAGENTA] = 0x6,
+    [FB_COLOR_YELLOW] = 0x4,
+    [FB_COLOR_BWHITE] = 0x8,
 };
 
 static void fb_fs_init_console(void)
