e79f80d74b319e9cb6140be3fa17080f6b3ded13
v1ctor <av1ctor@yahoo.com.br>
2015-10-18 12:29:40 -0200

    fixed: cursor on/off

--- src/rtlib/js/io_locate.c
+++ src/rtlib/js/io_locate.c
@@ -5,9 +5,28 @@
 
 int fb_ConsoleLocate( int row, int col, int cursor )
 {
-	EM_ASM_ARGS({
-        __fb_rtlib.console.pos_set($0, $1);
-    }, row-1, col-1);
+	if( row > 0 || col > 0 )
+    {
+        EM_ASM_ARGS({
+            __fb_rtlib.console.pos_set($0, $1);
+        }, row-1, col-1);
+    }
+
+    if( cursor != -1 )
+    {
+        if( cursor == 0 )
+        {
+            EM_ASM({
+                __fb_rtlib.console.cursor_hide();
+            });
+        }
+        else
+        {
+            EM_ASM({
+                __fb_rtlib.console.cursor_show();
+            });
+        }
+    }
 
 	return (col & 0xFF) | ((row & 0xFF) << 8);
 }
