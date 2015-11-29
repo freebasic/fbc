344389a8f473ae907a44e0656523d92c0d6a7aca
dkl <daniel.c.klauer@web.de>
2015-11-29 18:20:51 +0100

    Revert bad Get# wstring changes
    In commit cab466533799 I changed Get#(wstring) to convert the loaded bytes
    to wstring characters, but that's not how Get# should work:
    
    It should just fill the buffer with the raw bytes. That's also what it
    does for other data types like Integer variables - it will load
    sizeof(integer) bytes, not just 1 byte.
    
    This makes sense, since Get# is intended for use with Open For Binary.
    The conversion to wstring chars is something that Input# with Open For
    Input (i.e. text mode) would do.
    
    That Get# change also broke part of fbc's UTF file reading, because it
    uses Get#(wstring) (in hUTF16LEToUTF16LE() and hUTF32LEToUTF32LE()) and
    relies on it to load raw bytes as-is without any conversion.

--- src/rtlib/file_get_wstr.c
+++ src/rtlib/file_get_wstr.c
@@ -11,29 +11,44 @@ int fb_FileGetWstrEx
 		size_t *bytesread
 	)
 {
-    int res;
-
 	if( bytesread )
 		*bytesread = 0;
 
-    if( !FB_HANDLE_USED(handle) )
+	if( !FB_HANDLE_USED(handle) || !dst || dst_chars < 0 )
 		return fb_ErrorSetNum( FB_RTERROR_ILLEGALFUNCTIONCALL );
 
-	if( (dst != NULL) && (dst_chars > 1) ) {
-		/* read dst_chars - 1 chars, then add null-terminator */
-		size_t chars;
-		res = fb_FileGetDataEx( handle, pos, (void *)dst, dst_chars - 1, &chars, TRUE, TRUE );
-		if (res == FB_RTERROR_OK) {
-			dst[chars] = _LC('\0'); /* null-terminator */
-			if (bytesread)
-				*bytesread = chars * sizeof(FB_WCHAR);
-		}
-	} else {
-		/* no/empty destination string */
-		res = fb_ErrorSetNum( FB_RTERROR_ILLEGALFUNCTIONCALL );
+	/* may have to detect the length if given a dereferenced wstring ptr */
+	if( dst_chars == 0 ) {
+		dst_chars = fb_wstr_Len( dst ) + 1;
 	}
 
-	return res;
+	/* need room for at least 1 wchar and the null terminator */
+	/* (Get# on a wstring * 1, i.e. just room for the null terminator, is not supported,
+	   same as for [z]strings) */
+	if( dst_chars < 2 )
+		return fb_ErrorSetNum( FB_RTERROR_ILLEGALFUNCTIONCALL );
+
+	/* Fill wchar buffer with raw bytes from the file. */
+	/* We request to read in multiples of sizeof(wchar), but EOF can be
+	   reached at an odd number of bytes - fb_DevFileRead() will fill the
+	   remainder with zeroes at least. */
+	size_t rawbytesread;
+	int res = fb_FileGetDataEx( handle, pos, (void *)dst, (dst_chars - 1) * sizeof(FB_WCHAR), &rawbytesread, TRUE, FALSE );
+	if( res != FB_RTERROR_OK )
+		return res;
+
+	if (bytesread)
+		*bytesread = rawbytesread;
+
+	/* Add null-terminator */
+	int extra = rawbytesread % sizeof(FB_WCHAR);
+	if (extra > 0) {
+		rawbytesread += sizeof(FB_WCHAR) - extra; /* round up */
+	}
+	DBG_ASSERT( (rawbytesread % sizeof(FB_WCHAR)) == 0 );
+	dst[rawbytesread / sizeof(FB_WCHAR)] = _LC('\0');
+
+	return FB_RTERROR_OK;
 }
 
 FBCALL int fb_FileGetWstr( int fnum, int pos, FB_WCHAR *dst, ssize_t dst_chars )
