freebasic/fbc

commit 150a7d626ad038d9a658f07caebd92eeb131cc2a

Windows console input: Handle keypress repeat count
(probably regression from 93b7bd89bd8cf7de119ca96d3111a3fa8c797fd1)

Multiple equal successive keypress events can be merged into one by the
system. We need to handle the repeat count for that case. Example to
trigger such input event on my Windows 7:

#include "windows.bi"

dim ip as INPUT_
ip.type = INPUT_KEYBOARD
ip.ki.wVk = VK_A

for i as integer = 0 to 9
	'' keydown
	SendInput(1, @ip, sizeof(ip))
next

'' keyup
ip.ki.dwFlags = KEYEVENTF_KEYUP
SendInput(1, @ip, sizeof(ip))

src/rtlib/win32/io_input.c
@@ -263,7 +263,7 @@ void fb_hConsolePutBackEvents( void )
     FB_UNLOCK();
 }
 
-static void fb_hConsoleProcessKeyEvent( KEY_EVENT_RECORD *event )
+static void fb_hConsoleProcessKeyEvent( const KEY_EVENT_RECORD *event )
 {
     int KeyCode;
     int ValidKeyStatus, ValidKeys, AddScratchPadKey = FALSE;
@@ -403,7 +403,9 @@ int fb_ConsoleProcessEvents( void )
             switch ( ir.EventType ) {
             case KEY_EVENT:
                 if( ir.Event.KeyEvent.bKeyDown && ir.Event.KeyEvent.wRepeatCount != 0 ) {
-                    fb_hConsoleProcessKeyEvent( &ir.Event.KeyEvent );
+                    for (size_t i = 0; i < ir.Event.KeyEvent.wRepeatCount; i++) {
+                        fb_hConsoleProcessKeyEvent( &ir.Event.KeyEvent );
+                    }
                 } else if( !ir.Event.KeyEvent.bKeyDown ) {
                     fb_hConsoleProcessKeyEvent( &ir.Event.KeyEvent );
                 }

commit 23bd26e05b0e8fc784338d5242ccd37558a42b9f

rtlib: Restore C89-compatibility
(problem introduced in 150a7d62)

src/rtlib/win32/io_input.c
@@ -403,7 +403,8 @@ int fb_ConsoleProcessEvents( void )
             switch ( ir.EventType ) {
             case KEY_EVENT:
                 if( ir.Event.KeyEvent.bKeyDown && ir.Event.KeyEvent.wRepeatCount != 0 ) {
-                    for (size_t i = 0; i < ir.Event.KeyEvent.wRepeatCount; i++) {
+                    size_t i;
+                    for (i = 0; i < ir.Event.KeyEvent.wRepeatCount; i++) {
                         fb_hConsoleProcessKeyEvent( &ir.Event.KeyEvent );
                     }
                 } else if( !ir.Event.KeyEvent.bKeyDown ) {