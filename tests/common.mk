768660fd1afa04546679f0f8fca8c3862dc45d46
Ralph Versteegen <teeemcee@gmail.com>
2016-10-19 03:10:05 +1300

    tests: Add TARGET config option to makefile. Only tested for cunit tests
    This allows cross-compiling the fbc-tests binary. make still tries running
    the built binary regardless.
    
    I renamed the previous TARGET variable to TARGET_OS, and cleaned up
    common.mk a bit (bad indentation; 'unix' is now used instead of 'linux').

--- tests/common.mk
+++ tests/common.mk
@@ -1,7 +1,8 @@
 # common.mk
 # This file is part of the FreeBASIC test suite
 #
-# Guess HOST and TARGET if not already set
+# Guess HOST and TARGET_OS if not already set
+# HOST and TARGET_OS both take possible values dos|unix|win32
 # 
 
 HOST :=
@@ -9,35 +10,35 @@ ifeq ($(OS),DOS)
 	HOST := dos
 else
 	ifeq ($(OS),Windows_NT)
-   		HOST := win32
+		HOST := win32
 	else
-   		ifdef WINDIR
-   			HOST := win32
+		ifdef WINDIR
+			HOST := win32
 		else
-       		ifdef windir
-       			HOST := win32
-		else
-		ifdef HOME
-			HOST := linux
+			ifdef windir
+				HOST := win32
+			else
+				ifdef HOME
+					HOST := unix
+				endif
+			endif
 		endif
-       		endif
-   		endif
 	endif
 endif
 
-ifndef TARGET
+ifndef TARGET_OS
 	ifndef HOST
-		CHECKHOST_MSG := $(error error: TARGET not defined and HOST couldn't be guessed)
+		CHECKHOST_MSG := $(error error: TARGET_OS not defined and HOST couldn't be guessed)
 	else
 		CHECKHOST_MSG :=
 	endif
-	TARGET := $(HOST)
+	TARGET_OS := $(HOST)
 endif
 
 # set default command names
 # 
 
-ifeq ($(TARGET),linux)
+ifeq ($(TARGET_OS),unix)
     EXEEXT :=
 else
     EXEEXT := .exe
