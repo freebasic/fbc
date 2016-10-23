a3ca4b4fcf25f610dc02034e83c40eee937c91d9
Ralph Versteegen <teeemcee@gmail.com>
2016-10-23 15:47:54 +1300

    tests: fix the ability to cross-compile from/to win32/dos
    TARGET_OS can now be determined from TARGET, and there are separate
    EXEEXT and TARGET_EXEEXT

--- tests/common.mk
+++ tests/common.mk
@@ -1,8 +1,11 @@
 # common.mk
 # This file is part of the FreeBASIC test suite
 #
-# Guess HOST and TARGET_OS if not already set
-# HOST and TARGET_OS both take possible values dos|unix|win32
+# Guess HOST and TARGET_OS if not already set;
+# it would far cleaner and robust to reuse the detection code in root makefile,
+# but we our requirements here are much simpler.
+# HOST and TARGET_OS both take possible values dos|unix|win32.
+# OS has possible values DOS and Windows_NT.
 # 
 
 HOST :=
@@ -27,19 +30,37 @@ else
 endif
 
 ifndef TARGET_OS
-	ifndef HOST
-		CHECKHOST_MSG := $(error error: TARGET_OS not defined and HOST couldn't be guessed)
+	triplet := $(subst -, ,$(TARGET))
+	ifdef TARGET
+		ifneq ($(filter djgpp%,$(triplet)),)
+			TARGET_OS := dos
+		else ifneq ($(filter msdos%,$(triplet)),)
+			TARGET_OS := dos
+		else ifneq ($(filter mingw%,$(triplet)),)
+			TARGET_OS := win32
+		else
+			TARGET_OS := unix
+		endif
 	else
-		CHECKHOST_MSG :=
+		ifndef HOST
+			CHECKHOST_MSG := $(error error: TARGET_OS not defined and HOST couldn't be guessed)
+		else
+			CHECKHOST_MSG :=
+		endif
+		TARGET_OS := $(HOST)
 	endif
-	TARGET_OS := $(HOST)
 endif
 
 # set default command names
 # 
 
-ifeq ($(TARGET_OS),unix)
+ifeq ($(HOST),unix)
     EXEEXT :=
 else
     EXEEXT := .exe
+endif
+ifeq ($(TARGET_OS),unix)
+    TARGET_EXEEXT :=
+else
+    TARGET_EXEEXT := .exe
 endif
