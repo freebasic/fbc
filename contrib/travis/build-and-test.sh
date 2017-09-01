8ac4648ba95d996f0ab5ff6c02a3aa24a60536eb
Daniel Klauer <daniel.c.klauer@web.de>
2017-09-01 23:35:46 +0200

    travis: build 32bit fbc too

--- contrib/travis/build-and-test.sh
+++ contrib/travis/build-and-test.sh
@@ -3,6 +3,10 @@ set -ex
 
 source "$(dirname "$0")/bootstrap-settings.sh"
 
+if [ "$1" = "32" ]; then
+	echo "CC = gcc -m32" > config.mk
+	echo "TARGET_ARCH = x86" >> config.mk
+fi
 make -j$(nproc) FBC="$bootstrap_package/bin/fbc -i $bootstrap_package/inc" </dev/null
 mv bin/fbc bin/fbc1
 make -j$(nproc) clean-compiler
