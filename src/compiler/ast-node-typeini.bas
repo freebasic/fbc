freebasic/fbc

commit 616dc23ddcf2194720aa86e21b5c2a84cc84c1a2

Fix emitting of CONST ref initializers (as opposed to OFFSETs)
The CONST code path in hFlushExprStatic() used the symbol type as-is,
instead of converting to pointer type for references. Thus the type
looked different and it attempted the astNewCONV() which could easily
fail (because the init expression is a pointer, while the symbol could
be anything, e.g. struct/string).

src/compiler/ast-node-typeini.bas
@@ -522,7 +522,15 @@ private sub hFlushExprStatic( byval n as ASTNODE ptr, byval basesym as FBSYMBOL 
 	if( sym = NULL ) then
 		sym = basesym
 	end if
+
 	var sdtype = symbGetType( sym )
+	var sfulldtype = symbGetType( sym )
+	if( symbIsRef( sym ) ) then
+		'' Initializers for references initialize the pointer,
+		'' not an object of the symbol's type.
+		sdtype = typeAddrOf( sdtype )
+		sfulldtype = typeAddrOf( sfulldtype )
+	end if
 
 	'' Get rhs expression
 	var expr = n->l
@@ -543,7 +551,7 @@ private sub hFlushExprStatic( byval n as ASTNODE ptr, byval basesym as FBSYMBOL 
 		else
 			'' different types?
 			if( edtype <> sdtype ) then
-				expr = astNewCONV( symbGetFullType( sym ), symbGetSubtype( sym ), expr, AST_CONVOPT_DONTCHKPTR )
+				expr = astNewCONV( sfulldtype, symbGetSubtype( sym ), expr, AST_CONVOPT_DONTCHKPTR )
 				assert( expr <> NULL )
 			end if
 

tests/dim/byref.bas
@@ -608,4 +608,25 @@ SUITE( fbc_tests.dim_.byref_ )
 		externs.globalExterns_proc
 	END_TEST
 
+	TEST_GROUP( globalConstantInitWithoutOffset )
+		type UDT1
+			dummy as integer
+		end type
+
+		type UDT2
+			dummy as integer
+			static byref r1 as UDT1
+			static byref r2 as UDT1
+		end type
+
+		'' Initializer for global ref var using a constant that is not an offset
+		dim byref UDT2.r1 as UDT1 = *cptr(UDT1 ptr, 0)
+		dim byref UDT2.r2 as UDT1 = *cptr(UDT1 ptr, 123)
+
+		TEST( default )
+			CU_ASSERT( @UDT2.r1 = 0 )
+			CU_ASSERT( @UDT2.r2 = 123 )
+		END_TEST
+	END_TEST_GROUP
+
 END_SUITE

commit d0150787562621cec3d9ae44766fc9f10a546b6e

Fix hFlushExprStatic() to use full type again (regression from 616dc23d)
I can't think of a test case for this though, CONSTness should have been
checked before already, it shouldn't matter here when emitting.
Also the full type is only used for an astNewCONV() which allows casting
away CONSTness etc. anyways.

src/compiler/ast-node-typeini.bas
@@ -524,7 +524,7 @@ private sub hFlushExprStatic( byval n as ASTNODE ptr, byval basesym as FBSYMBOL 
 	end if
 
 	var sdtype = symbGetType( sym )
-	var sfulldtype = symbGetType( sym )
+	var sfulldtype = symbGetFullType( sym )
 	if( symbIsRef( sym ) ) then
 		'' Initializers for references initialize the pointer,
 		'' not an object of the symbol's type.