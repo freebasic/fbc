231883ccd4d9b94dafa00e504fa2166cd7e8b68c
coderJeff <coder@execulink.com>
2019-09-26 11:58:16 -0400

    rtlib: FileTruncate, call SetEndOfFile on mingw

--- src/rtlib/win32/file_htruncate.c
+++ src/rtlib/win32/file_htruncate.c
@@ -2,15 +2,35 @@
 
 #include "../fb.h"
 #include <unistd.h>
+#include <windows.h>
+
+/*
+    rely on mingw-w64 having FTRUNCATE_DEFINED defined
+    to let us know that ftruncate64 is defined.
+
+    Otherwise, just call the windows API SetEndOfFile
+
+    Perhaps in an updated version of mingw(org) we can
+    remove the conditional compilcation here and use
+    only ftruncate/ftruncate64.  Would be nice if mingw
+    supports _FILE_OFFSET_BITS in a later version than
+    our current setup
+*/
 
 int fb_hFileTruncateEx( FILE *f )
 {
-	fb_off_t pos;
 
+#if defined( FTRUNCATE_DEFINED )
+	fb_off_t pos;
 	pos = ftello( f );
-	if( ftruncate( fileno(f), pos ) != 0 ) {
+	if( ftruncate64( fileno(f), pos ) != 0 ) {
 		return fb_ErrorSetNum( FB_RTERROR_FILEIO );
 	}
+#else
+	if( SetEndOfFile( (HANDLE)get_osfhandle( fileno( f ) ) ) == 0 ) {
+		return fb_ErrorSetNum( FB_RTERROR_FILEIO );
+	}
+#endif
 
 	return fb_ErrorSetNum( FB_RTERROR_OK );
 	
