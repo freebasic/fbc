7447fdf3e910f69b654c0d87f56cd46c0dd4347a
coderJeff <coder@execulink.com>
2020-10-18 09:15:55 -0400

    gfxlib2: linux-arm framebuffer and vga16_blitter
    - enable frame buffer for linux-arm targets
    - add vga16_blitter for linux-arm targets to pack 2 pixels per byte and write to linear frame buffer

--- src/gfxlib2/linux/gfx_driver_fbdev.c
+++ src/gfxlib2/linux/gfx_driver_fbdev.c
@@ -89,6 +89,8 @@ static pthread_t thread;
 static pthread_mutex_t mutex;
 static pthread_cond_t cond;
 
+#if defined HOST_X86 || defined HOST_X86_64
+
 static void vga16_blitter(unsigned char *dest, int pitch)
 {
 	unsigned int color;
@@ -142,6 +144,46 @@ static void vga16_blitter(unsigned char *dest, int pitch)
 		source += __fb_gfx->pitch;
 	}
 }
+
+#else /* !( defined HOST_X86 || defined HOST_X86_64 ) */
+
+static void vga16_blitter(unsigned char *dest, int pitch)
+{
+	unsigned int c, color[2];
+	unsigned char buffer[fb_fbdev.w];
+	unsigned char *s, *source = __fb_gfx->framebuffer;
+	int x, y, offset, i;
+
+	for (y = 0; y < fb_fbdev.h; y++) {
+		if (__fb_gfx->dirty[y]) {
+			offset = 0;
+			s = source;
+			for (x = 0; x < fb_fbdev.w; x += 2) {
+				if (__fb_gfx->depth == 8) {
+					for( i = 0; i < 2; i++ )
+					{
+						c = __fb_gfx->device_palette[s[i]];
+						color[i] = color_conv[((c & 0xF0) >> 4) | ((c & 0xF000) >> 8) | ((c & 0xF00000) >> 12)];
+					}
+					buffer[offset] = (color[0] << 4) | color[1];
+				}
+				else
+				{
+					buffer[offset] = ((s[0] << 4) & 0xF0) | (s[1] & 0xF);
+				}
+
+				offset++;
+				s += 2;
+			}
+
+			fb_hMemCpy(dest, buffer, (fb_fbdev.w >> 1));
+		}
+		dest += pitch;
+		source += __fb_gfx->pitch;
+	}
+}
+
+#endif /* defined HOST_X86 || defined HOST_X86_64 */
 
 static void *driver_thread(void *arg)
 {
