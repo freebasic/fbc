82c58367fbfbb7e97b78705f0efbd11847a4cff7
Ralph Versteegen <teeemcee@gmail.com>
2023-06-25 02:40:52 +1200

    tests/emscripten: include contents of file/ and data/ for fbc-tests

--- tests/unit-tests.mk
+++ tests/unit-tests.mk
@@ -121,7 +121,8 @@ ifdef TARGET
 	FBC_LFLAGS += -target $(TARGET)
 endif
 ifeq ($(TARGET_OS),js)
-	FBC_LFLAGS += -Wl -sEXIT_RUNTIME
+# Copy the file/ and data/ directories into the in-memory FS
+	FBC_LFLAGS += -Wl -sASYNCIFY,-sEXIT_RUNTIME -Wl --preload-file,file,--preload-file,data
 endif
 
 ifeq ($(ENABLE_CHECK_BUGS),1)
