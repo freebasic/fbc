5ea776b6c69695738cdd93511a2c5196c85cb78c
Ralph Versteegen <teeemcee@gmail.com>
2023-07-25 03:30:20 +1200

    tests/emscripten: fix some failures due to test problems rather than bugs
    -boolean/* and wstring/* test files weren't included in fbc-tests.js
    -wasm/emscripten uses 8-byte padding for longints and doubles

--- tests/unit-tests.mk
+++ tests/unit-tests.mk
@@ -122,7 +122,7 @@ ifdef TARGET
 endif
 ifeq ($(TARGET_OS),js)
 # Copy the file/ and data/ directories into the in-memory FS
-	FBC_LFLAGS += -Wl -sEXIT_RUNTIME -Wl --preload-file,file,--preload-file,data
+	FBC_LFLAGS += -Wl -sEXIT_RUNTIME -Wl --preload-file,boolean,--preload-file,data,--preload-file,file,--preload-file,wstring
 endif
 
 ifeq ($(ENABLE_CHECK_BUGS),1)
