0ef0c1b3881bb10d0600e511ce5032b5a974c247
Jeff Marshall <coder@execulink.com>
2024-04-07 13:34:22 -0400

    profiler: rtlib: add fbrt1 profiler startup module
    - rtlib/static/fbrt1.c for profiler start-up and exit code
    - call fb_InitProfile() on startup
    - call fb_EndProfile(0) on exit
    - based on code removed by b696c61f38a65ae241b740672806702e0fd1f61a

---
+++ src/rtlib/static/fbrt1.c
@@ -0,0 +1,57 @@
+/* FB runtime initialization and cleanup for profiler
+
+   We use a global constructor and destructor for this. Where possible they
+   should run first/last respectively, such that it's safe for FB programs to
+   use the FB runtime from inside its own global ctors/dtors. */
+
+#include "../fb.h"
+
+FBCALL int fb_EndProfile( int errorlevel );
+FBCALL void fb_InitProfile( void );
+
+/* note: they must be static, or shared libraries in Linux would reuse the 
+		 same function */
+
+#if defined(HOST_DARWIN) || defined(HOST_ANDROID)
+	/* It seems like __attribute__((constructor(priority))) (or in general, ordering
+	   ctors/dtors across modules) isn't supported on Darwin/MacOSX, so we just use
+	   plain __attribute__((constructor)). */
+	__attribute__((constructor)) static void fb_hDoInit( void )
+	{
+		fb_hRtInit( );
+		fb_InitProfile();
+	}
+
+	__attribute__((destructor)) static void fb_hDoExit( void )
+	{
+		fb_EndProfile(0);
+		fb_hRtExit( );
+	}
+#else
+	static void fb_hDoInit( void )
+	{
+		fb_hRtInit( );
+		fb_InitProfile();
+	}
+
+	static void fb_hDoExit( void )
+	{
+		fb_EndProfile(0);
+		fb_hRtExit( );
+	}
+
+	/* This puts the init/exit global ctor/dtor for the rtlib in the sorted
+	   ctors/dtors section. A named section of .?tors.65435 = Priority(100).
+	   (65535 - 100 = 65435)
+
+	   This is what __attribute__((constructor(100))) would do; but that would also
+	   trigger a gcc warning, because priorities 0..100 are "reserved for the
+	   implementation", so we can't use that.
+
+	   GCC on GNU/Linux seems to use .init_array.<0-padded priority> to implement
+	    __attribute__((constructor(priority))) now (instead of
+	   .ctors.<65535 - priority>), but .ctors.* still works, so it's probably ok to
+	   keep using it. */
+	static void * priorityhDoInit __attribute__((section(".ctors.65435"), used)) = fb_hDoInit;
+	static void * priorityhDoExit __attribute__((section(".dtors.65435"), used)) = fb_hDoExit;
+#endif
